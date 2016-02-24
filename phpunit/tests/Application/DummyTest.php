2e905229a035c2bfef9a40dcd7a7f54de23f87dd
automatix <info@automatix.info>
2016-02-24 09:24:34 +0100

    PHPUnit integrated. Example test created.
        $ cd {project root}
        $ phpunit --configuration phpunit/phpunit.xml --testsuite "app-only"

---
+++ phpunit/tests/Application/DummyTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Application;
+
+class DummyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+    }
+}
