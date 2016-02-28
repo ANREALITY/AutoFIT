5e687f4535918d46757a5288eb70d7ccfe1abf41
automatix <info@automatix.info>
2016-02-28 16:05:25 +0100

    PHPUnit infrastructure: PHPUnit `Bootstrap` class commented and formatted; the `color` setting and the paths set correctly in the `phpunit.xml` template.

--- phpunit/Bootstrap.php
+++ phpunit/Bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Test;
 
 use Zend\Mvc\Application;
@@ -7,7 +6,6 @@ use Zend\Loader\AutoloaderFactory;
 use Zend\Mvc\Service\ServiceManagerConfig;
 use Zend\ServiceManager\ServiceManager;
 use Zend\Stdlib\ArrayUtils;
-
 error_reporting(E_ALL | E_STRICT);
 
 /**
@@ -15,34 +13,49 @@ error_reporting(E_ALL | E_STRICT);
  */
 class Bootstrap
 {
+
+    /**
+     *
+     * @var ServiceManager
+     */
     protected static $serviceManager;
 
+    /**
+     *
+     * @var Application
+     */
     protected static $application;
 
+    /**
+     *
+     * @var Application
+     */
     private $app;
 
     public static function init()
     {
-        $zf2ModulePaths = array(dirname(dirname(__DIR__)));
+        $zf2ModulePaths = array(
+            dirname(dirname(__DIR__))
+        );
         if (($path = static::findParentPath('vendor'))) {
             $zf2ModulePaths[] = $path;
         }
         if (($path = static::findParentPath('module')) !== $zf2ModulePaths[0]) {
             $zf2ModulePaths[] = $path;
         }
-
+        
         static::initAutoloader();
-
+        
         // use ModuleManager to load this module and it's dependencies
         $config = array(
             'module_listener_options' => array(
-                'module_paths' => $zf2ModulePaths,
+                'module_paths' => $zf2ModulePaths
             ),
-            'modules'                 => array(
+            'modules' => array(
                 'JuunaConnectApi'
             )
         );
-
+        
         $serviceManager = new ServiceManager(new ServiceManagerConfig());
         $serviceManager->setService('ApplicationConfig', $config);
         $serviceManager->get('ModuleManager')->loadModules();
@@ -79,9 +92,9 @@ class Bootstrap
     protected static function initAutoloader()
     {
         $vendorPath = static::findParentPath('vendor');
-
+        
         $zf2Path = getenv('ZF2_PATH');
-        if (!$zf2Path) {
+        if (! $zf2Path) {
             if (defined('ZF2_PATH')) {
                 $zf2Path = ZF2_PATH;
             } elseif (is_dir($vendorPath . '/ZF2/library')) {
@@ -90,36 +103,31 @@ class Bootstrap
                 $zf2Path = $vendorPath . '/zendframework/zendframework/library';
             }
         }
-
-        if (!$zf2Path) {
-            throw new \RuntimeException(
-                'Unable to load ZF2. Run `php composer.phar install` or'
-                . ' define a ZF2_PATH environment variable.'
-            );
+        
+        if (! $zf2Path) {
+            throw new \RuntimeException('Unable to load ZF2. Run `php composer.phar install` or' . ' define a ZF2_PATH environment variable.');
         }
-
+        
         if (file_exists($vendorPath . '/autoload.php')) {
             include $vendorPath . '/autoload.php';
         }
-
+        
         include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
-        AutoloaderFactory::factory(
-            array(
-                'Zend\Loader\StandardAutoloader' => array(
-                    'autoregister_zf' => true,
-                    'namespaces'      => array(
-                        __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
-                    ),
-                ),
+        AutoloaderFactory::factory(array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'autoregister_zf' => true,
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__
+                )
             )
-        );
+        ));
     }
 
     protected static function findParentPath($path)
     {
         $dir = __DIR__;
         $previousDir = '.';
-        while (!is_dir($dir . '/' . $path)) {
+        while (! is_dir($dir . '/' . $path)) {
             $dir = dirname($dir);
             if ($previousDir === $dir) {
                 return false;
@@ -143,38 +151,37 @@ class Bootstrap
     protected static function setUp($application)
     {
         // echo "\n====START SET UP====\n";
-
         $config = $application->getConfig();
-
+        
         /*
-        if (isset($config['integrationTest'])) {
-            $testConfig = $config['integrationTest'];
-            if (isset($testConfig['migrateScript'])) {
-                $dbConfig = $config['doctrine']['connection']['orm_default']['params'];
-                $options = '-c ' . $testConfig['createDatabaseFile'] . ' -s ' . $testConfig['updateDirectory'] . ' -d '
-                    . $dbConfig['dbname'] . ' -u ' . $dbConfig['user'] . ' -p ' . $dbConfig['password'];
-                echo "  -> running migrateScript:" . $testConfig['migrateScript'] . "\n";
-                echo exec($testConfig['migrateScript'] . ' ' . $options);
-            }
-        } else {
-            echo "WARNING: no integrationTest Configuration found\n";
-        }
-        */
-
+         * if (isset($config['integrationTest'])) {
+         * $testConfig = $config['integrationTest'];
+         * if (isset($testConfig['migrateScript'])) {
+         * $dbConfig = $config['doctrine']['connection']['orm_default']['params'];
+         * $options = '-c ' . $testConfig['createDatabaseFile'] . ' -s ' . $testConfig['updateDirectory'] . ' -d '
+         * . $dbConfig['dbname'] . ' -u ' . $dbConfig['user'] . ' -p ' . $dbConfig['password'];
+         * echo " -> running migrateScript:" . $testConfig['migrateScript'] . "\n";
+         * echo exec($testConfig['migrateScript'] . ' ' . $options);
+         * }
+         * } else {
+         * echo "WARNING: no integrationTest Configuration found\n";
+         * }
+         */
+        
         // echo "\n=====END SET UP=====\n";
     }
 
     protected static function tearDown($application)
     {
         // echo "\n===START TEAR DOWN===\n";
-
+        
         /*
-        $config = $application->getConfig();
-        $dbConfig = $config['doctrine']['connection']['orm_default']['params'];
-        $em = $application->getServiceManager()->get('Doctrine\ORM\EntityManager');
-        $em->getConnection()->exec('DROP DATABASE `'.$dbConfig['dbname'].'`');
-        */
-
+         * $config = $application->getConfig();
+         * $dbConfig = $config['doctrine']['connection']['orm_default']['params'];
+         * $em = $application->getServiceManager()->get('Doctrine\ORM\EntityManager');
+         * $em->getConnection()->exec('DROP DATABASE `'.$dbConfig['dbname'].'`');
+         */
+        
         // echo "\n====END TEAR DOWN====\n";
     }
 }
@@ -182,17 +189,14 @@ class Bootstrap
 // Setup autoloading
 require 'init_autoloader.php';
 
-if (!defined('APPLICATION_PATH')) {
+if (! defined('APPLICATION_PATH')) {
     define('APPLICATION_PATH', realpath(__DIR__ . '/../'));
 }
 
 $appConfig = include APPLICATION_PATH . '/config/application.config.php';
 
 if (file_exists(APPLICATION_PATH . '/config/application.config.integrationtest.php')) {
-    $appConfig = ArrayUtils::merge(
-        $appConfig,
-        include APPLICATION_PATH . '/config/application.config.integrationtest.php'
-    );
+    $appConfig = ArrayUtils::merge($appConfig, include APPLICATION_PATH . '/config/application.config.integrationtest.php');
 }
 
 // Some OS/Web Server combinations do not glob properly for paths unless they
@@ -205,7 +209,6 @@ if (isset($appConfig['module_listener_options']['config_glob_paths'])) {
     }
 }
 
-
 // Run the application!
 $application = Application::init($appConfig);
 
@@ -214,5 +217,5 @@ $bootstrap = new Bootstrap($application);
 Bootstrap::setApplication($application);
 Bootstrap::setServiceManager($application->getServiceManager());
 
-//Bootstrap::init();
+// Bootstrap::init();
 Bootstrap::chroot();
