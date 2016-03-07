3ae1b74c900bc4168753594676f17a8787d538f8
automatix <info@automatix.info>
2016-03-07 13:27:51 +0100

    #10 `FileTransferRequestService` implemented.

---
+++ module/FileTransferRequest/src/FileTransferRequest/Factory/FileTransferRequestServiceFactory.php
@@ -0,0 +1,21 @@
+<?php
+// Filename: /module/FileTransferRequest/src/FileTransferRequest/Factory/PostServiceFactory.php
+namespace FileTransferRequest\Factory;
+
+use FileTransferRequest\Service\FileTransferRequestService;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class FileTransferRequestServiceFactory implements FactoryInterface
+{
+    /**
+     * Create service
+     *
+     * @param ServiceLocatorInterface $serviceLocator            
+     * @return mixed
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        return new FileTransferRequestService($serviceLocator->get('FileTransferRequest\Mapper\FileTransferRequestMapperInterface'));
+    }
+}
---
+++ module/FileTransferRequest/src/FileTransferRequest/Service/FileTransferRequestService.php
@@ -0,0 +1,54 @@
+<?php
+namespace FileTransferRequest\Service;
+
+use FileTransferRequest\Mapper\FileTransferRequestMapperInterface;
+use DbSystel\DataObject\FileTransferRequest;
+
+class FileTransferRequestService implements FileTransferRequestServiceInterface
+{
+
+    /**
+     *
+     * @var \FileTransferRequest\Mapper\FileTransferRequestMapperInterface
+     */
+    protected $fileTransferRequestMapper;
+
+    /**
+     *
+     * @param FileTransferRequestMapperInterface $fileTransferRequestMapper            
+     */
+    public function __construct(FileTransferRequestMapperInterface $fileTransferRequestMapper)
+    {
+        $this->fileTransferRequestMapper = $fileTransferRequestMapper;
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     *
+     */
+    public function findAllFileTransferRequests()
+    {
+        return $this->fileTransferRequestMapper->findAll();
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     *
+     */
+    public function findFileTransferRequest($id)
+    {
+        return $this->fileTransferRequestMapper->find($id);
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     *
+     */
+    public function saveFileTransferRequest(FileTransferRequest $fileTransferRequest)
+    {
+        return $this->fileTransferRequestMapper->save($fileTransferRequest);
+    }
+}
