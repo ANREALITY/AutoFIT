a1f06fdd33f1363fd1f3152c6b1d0fd73ea6bce3
automatix <info@automatix.info>
2016-03-07 15:10:16 +0100

    #10 `FileTransferRequest` Mapper implemented (for saving data).

---
+++ module/FileTransferRequest/src/FileTransferRequest/Factory/FileTransferRequestMapperFactory.php
@@ -0,0 +1,23 @@
+<?php
+namespace FileTransferRequest\Factory;
+
+use FileTransferRequest\Mapper\FileTransferRequestMapper;
+use DbSystel\DataObject\FileTransferRequest;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class FileTransferRequestMapperFactory implements FactoryInterface
+{
+
+    /**
+     * Create service
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     *
+     * @return mixed
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        return new FileTransferRequestMapper($serviceLocator->get('Zend\Db\Adapter\Adapter'), $serviceLocator->get('HydratorManager')->get('DbSystel\Hydrator\FileTransferRequestHydrator'), new FileTransferRequest());
+    }
+}
