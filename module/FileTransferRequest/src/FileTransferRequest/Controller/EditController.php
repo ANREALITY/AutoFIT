3537f545f420f7c829b06ac1383deb0e46cdbc4c
automatix <info@automatix.info>
2016-03-07 13:19:12 +0100

    #10 Minor formatting improvements for `EditController`.

--- module/FileTransferRequest/src/FileTransferRequest/Controller/EditController.php
+++ module/FileTransferRequest/src/FileTransferRequest/Controller/EditController.php
@@ -1,19 +1,19 @@
 <?php
+
 namespace FileTransferRequest\Controller;
 
-use FileTransferRequest\Service\FileTransferRequestService;
-use Zend\Form\FormInterface;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use FileTransferRequest\Service\FileTransferRequestServiceInterface;
 
 class EditController extends AbstractActionController
 {
 
-    protected $fileTransferRequestService;
+    protected $fileTransferRequestService = null;
 
-    protected $fileTransferRequestForm;
+    protected $fileTransferRequestForm = null;
 
-    public function __construct(FileTransferRequestService $fileTransferRequestService, FormInterface $fileTransferRequestForm)
+    public function __construct(FileTransferRequestServiceInterface $fileTransferRequestService, \Zend\Form\FormInterface $fileTransferRequestForm)
     {
         $this->fileTransferRequestService = $fileTransferRequestService;
         $this->fileTransferRequestForm = $fileTransferRequestForm;
@@ -22,26 +22,26 @@ class EditController extends AbstractActionController
     public function createRequestAction()
     {
         $request = $this->getRequest();
-        
+
         if ($request->isPost()) {
             $this->fileTransferRequestForm->setData($request->getPost());
-            
+
             if ($this->fileTransferRequestForm->isValid()) {
                 try {
-                    
+
                     // \Zend\Debug\Debug::dump($this->fileTransferRequestForm->getData());die();
 
                     // $fieldsetData = $this->fileTransferRequestForm->getFieldsets()['billing-fieldset'];
                     $test = $this->fileTransferRequestForm->getData();
                     $this->fileTransferRequestService->saveFileTransferRequest($this->fileTransferRequestForm->getData());
-                    
+
                     return $this->redirect()->toRoute('file-transfer-request');
                 } catch (\Exception $e) {
                     // Some DB Error happened, log it and let the user know
                 }
             }
         }
-        
+
         return new ViewModel(array(
             'form' => $this->fileTransferRequestForm
         ));
