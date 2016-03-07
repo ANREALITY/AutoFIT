924c78ce06b711405f4959bb6dcad6a44d271553
automatix <info@automatix.info>
2016-03-07 13:33:20 +0100

    #10 `UserMapper` implemented.

---
+++ module/FileTransferRequest/src/FileTransferRequest/Factory/UserMapperFactory.php
@@ -0,0 +1,23 @@
+<?php
+namespace FileTransferRequest\Factory;
+
+use FileTransferRequest\Mapper\UserMapper;
+use DbSystel\DataObject\User;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class UserMapperFactory implements FactoryInterface
+{
+
+    /**
+     * Create service
+     *
+     * @param ServiceLocatorInterface $serviceLocator            
+     *
+     * @return mixed
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        return new UserMapper($serviceLocator->get('Zend\Db\Adapter\Adapter'), $serviceLocator->get('HydratorManager')->get('DbSystel\Hydrator\UserHydrator'), new User());
+    }
+}
