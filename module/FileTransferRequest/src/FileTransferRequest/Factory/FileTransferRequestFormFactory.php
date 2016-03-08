e922fe75c4a3ecef09778824f36f87527700aa42
automatix <info@automatix.info>
2016-03-08 13:04:35 +0100

    #3, #4 Form `submit` became `button`. Some minor formatting changes.

--- module/FileTransferRequest/src/FileTransferRequest/Factory/FileTransferRequestFormFactory.php
+++ module/FileTransferRequest/src/FileTransferRequest/Factory/FileTransferRequestFormFactory.php
@@ -16,31 +16,38 @@ class FileTransferRequestFormFactory implements FactoryInterface
         $billingFieldset = $serviceLocator->get('FileTransferRequest\Form\Fieldset\BillingFieldset');
         $sourceFieldset = $serviceLocator->get('FileTransferRequest\Form\Fieldset\SourceFieldset');
         $targetFieldset = $serviceLocator->get('FileTransferRequest\Form\Fieldset\TargetFieldset');
-
-        $form->add(array(
-            'name' => 'billing',
-            'type' => 'FileTransferRequest\Form\Fieldset\BillingFieldset',
-            'options' => array(),
-            'use_as_base_fieldset' => true
-            // 'targetObject' =>
-        ));
-        $form->add(array(
-            'type' => 'FileTransferRequest\Form\Fieldset\SourceFieldset',
-            'name' => 'source',
-        ));
-        $form->add(array(
-            'type' => 'FileTransferRequest\Form\Fieldset\TargetFieldset',
-            'name' => 'target',
-        ));
-        $form->add(array(
-            'type' => 'submit',
-            'name' => 'submit',
-            'attributes' => array(
-                'value' => _('send'),
-                'class' => 'btn btn-default'
-            ),
-        ));
-
+        
+        $form->add(
+            array(
+                'name' => 'billing',
+                'type' => 'FileTransferRequest\Form\Fieldset\BillingFieldset',
+                'options' => array(),
+                'use_as_base_fieldset' => true
+            ))
+        // 'targetObject' =>
+        ;
+        $form->add(
+            array(
+                'type' => 'FileTransferRequest\Form\Fieldset\SourceFieldset',
+                'name' => 'source'
+            ));
+        $form->add(
+            array(
+                'type' => 'FileTransferRequest\Form\Fieldset\TargetFieldset',
+                'name' => 'target'
+            ));
+        $form->add(
+            array(
+                'type' => 'button',
+                'name' => 'submit',
+                'options' => array(
+                    'label' => _('send')
+                ),
+                'attributes' => array(
+                    'class' => 'btn btn-default'
+                )
+            ));
+        
         return $form;
     }
 }
