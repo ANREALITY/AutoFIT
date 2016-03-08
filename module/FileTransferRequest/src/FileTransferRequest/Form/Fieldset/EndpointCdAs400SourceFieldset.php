5a0198caa1c170a7f9f61e8cc0828539f79ac10a
automatix <info@automatix.info>
2016-03-08 17:40:14 +0100

    #4 The CD AS400 fieldsets implemented.

--- module/FileTransferRequest/src/FileTransferRequest/Form/Fieldset/EndpointCdAs400SourceFieldset.php
+++ module/FileTransferRequest/src/FileTransferRequest/Form/Fieldset/EndpointCdAs400SourceFieldset.php
@@ -3,5 +3,14 @@ namespace FileTransferRequest\Form\Fieldset;
 
 class EndpointCdAs400SourceFieldset extends EndpointCdAs400Fieldset
 {
-    
+    public function __construct($name = null, $options = array())
+    {
+        parent::__construct($name, $options);
+
+        // Hydrator and Prototype are set in the factory.
+        // $this->setHydrator(new ClassMethods(false));
+        // $this->setObject(new FileTransferRequest());
+
+        $this->setLabel(_('Source') . ' ' . _('AS400'));
+    }
 }
