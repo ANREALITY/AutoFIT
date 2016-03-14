f33763be1c0404384b821af62e8ef0968bf8b530
automatix <info@automatix.info>
2016-03-14 01:22:11 +0100

    #12 `PhysicalConnectionHydratorFactory` corrected.

--- vendor/db-systel/dbs-common-lib/src/Hydrator/Factory/PhysicalConnectionHydratorFactory.php
+++ vendor/db-systel/dbs-common-lib/src/Hydrator/Factory/PhysicalConnectionHydratorFactory.php
@@ -7,7 +7,7 @@ use DbSystel\DataObject\LogicalConnection;
 use DbSystel\Hydrator\Strategy\Entity\GenericEntityStrategy;
 use DbSystel\Hydrator\Strategy\Entity\GenericCollectionStrategy;
 use Zend\Hydrator\NamingStrategy\MapNamingStrategy;
-use DbSystel\DataObject\EndpointCdTandem;
+use DbSystel\DataObject\EndpointCdAs400;
 
 class PhysicalConnectionHydratorFactory implements FactoryInterface
 {
@@ -23,11 +23,11 @@ class PhysicalConnectionHydratorFactory implements FactoryInterface
     {
         $physicalConnectionHydrator = $serviceLocator->get('Zend\Hydrator\ClassMethods');
         $logicalConnectionHydrator = $serviceLocator->get('Zend\Hydrator\ClassMethods');
-        $endpointHydrator = $serviceLocator->get('DbSystel\Hydrator\EndpointCdTandemHydrator');
+        $endpointHydrator = $serviceLocator->get('DbSystel\Hydrator\EndpointCdAs400Hydrator');
 
         $physicalConnectionHydrator->addStrategy('logical_connection', new GenericEntityStrategy($logicalConnectionHydrator, new LogicalConnection()));        // @todo AbstractFactory needed!!!
         // $physicalConnectionHydrator->addStrategy('endpoints', new GenericCollectionStrategy($endpointHydrator, new AbstractEndpoint()));
-        $physicalConnectionHydrator->addStrategy('endpoints', new GenericCollectionStrategy($endpointHydrator, new EndpointCdTandem()));
+        $physicalConnectionHydrator->addStrategy('endpoints', new GenericCollectionStrategy($endpointHydrator, new EndpointCdAs400()));
 
         $namingStrategy = new MapNamingStrategy(array(
             'logical_connection' => 'logicalConnection',
