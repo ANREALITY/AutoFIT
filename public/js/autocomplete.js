06bf318b088df18c977f4a12afca8e175c55184c
automatix <info@automatix.info>
2016-03-28 17:17:06 +0200

    #13 Issue with adding autocomplete entries (instead of replacing them) resolved.Service invoice position field autocomplete works!

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -10,13 +10,22 @@ $(function() {
 			type:'remote'
 		}],	 
 	});
+});
+/**
+ * Autocompletion fro the field order-service-invoice-position-basic-number.
+ */
+$(function() {
 	$("#order-service-invoice-position-basic-number").autocomplete({
 		minLength: 3,
 		limit: 5,
-		valueKey:'number',
-		source : [{
-			url:"/order/ajax/provide-service-invoice-positions-basic?data[number]=%QUERY%&data[application_technical_short_name]=" + $('#order-application-number').val(),
-			type:'remote'
+		appendMethod:'replace',
+		closeOnBlur: true,
+		source : [
+		    function(query, add) {
+		    	orderApplicationNumber = $('#order-application-number').val();
+				$.getJSON("/order/ajax/provide-service-invoice-positions-basic?data[number]=" + query + "&data[application_technical_short_name]=" + orderApplicationNumber, function(response) {
+					add(response);
+				})
 		}],	 
 	});
 });
