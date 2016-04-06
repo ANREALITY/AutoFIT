3987e33c4179b0b7c42ffeb41ab3c391c40894bc
automatix <info@automatix.info>
2016-04-06 11:15:26 +0200

    #66 Autocomplete delay implemented for application number field.

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -5,20 +5,33 @@ var updateKeyPressed = function() {
 	keyPressed = new Date();
 }
 /**
- * Autocompletion fro the field order-application-number.
+ * Autocompletion for the field order-application-number.
  */
 $(function() {
 	$("#order-application-number").autocomplete({
 		minLength: 3,
 		limit: 5,
-		source : [{
-			url:"/order/ajax/provide-applications?data[technical_short_name]=%QUERY%",
-			type:'remote'
-		}],	 
+		appendMethod:'replace',
+		closeOnBlur: true,
+		source : [
+			function(query, add) {
+				var currentDate = new Date();
+				setInterval(function() {
+					if (currentDate.getTime() > keyPressed.getTime()) {
+						orderApplicationNumber = $('#order-application-number').val();
+						$.getJSON("/order/ajax/provide-applications?data[technical_short_name]=" + query, function(response) {
+							add(response);
+						});
+						updateKeyPressed();
+					}
+				}, 1000);
+			}
+		],
 	});
 });
+
 /**
- * Autocompletion fro the field order-service-invoice-position-basic-number.
+ * Autocompletion for the field order-service-invoice-position-basic-number.
  */
 $(function() {
 	$("#order-service-invoice-position-basic-number").autocomplete({
@@ -43,7 +56,7 @@ $(function() {
 	});
 });
 /**
- * Autocompletion fro the field order-service-invoice-position-personal-number.
+ * Autocompletion for the field order-service-invoice-position-personal-number.
  */
 $(function() {
 	$("#order-service-invoice-position-personal-number").autocomplete({
