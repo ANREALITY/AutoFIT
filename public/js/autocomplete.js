c32febe4878d933cadaebb9691fceb4e9ae82971
automatix <info@automatix.info>
2016-04-07 15:58:46 +0200

    #23 The code "Autocomplete like Google" based javaScript replaced by autocomplete based on "jQuery UI Autocomplete" based code. The autocomplete mechanism started now directly `onFocus`. The minimum required number of the characters typed in is configurable and can be `0` (`minLength : 0`). For the service invoice fields it is `0`, for the application number field it's `3`. A delay is built in. So that the request is not sent after every futher typed in character, but only when the user hasn't typed for `300ms`.

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -1,64 +1,77 @@
-// global variable
-var keyPressed = new Date();
-// call where you're watching for pressed keys.
-var updateKeyPressed = function() {
-	keyPressed = new Date();
-}
 /**
  * Autocompletion for the field order-application-number.
  */
 $(function() {
-	$("#order-application-number").autocomplete({
-		minLength: 3,
-		limit: 5,
-		appendMethod:'replace',
-		closeOnBlur: true,
-		source : [
-			function(query, add) {
-				orderApplicationNumber = $('#order-application-number').val();
-				$.getJSON("/order/ajax/provide-applications?data[technical_short_name]=" + query, function(response) {
-					add(response);
-				});
+	$("#order-application-number")
+		.autocomplete({
+			autoFocus : true,
+			delay : 500,
+			minLength : 3,
+			source : function(request, response) {
+				$.get(
+					"/order/ajax/provide-applications?"
+					+ "data[technical_short_name]=" + request.term,
+					{},
+					function(data) {
+						response(data.slice(0, 10));
+					}
+				);
 			}
-		],
+		}).on('focus', function(event) {
+			console.log(new Date());
+			console.log($(this));
+		$(this).autocomplete("search", this.value);
 	});
 });
-
 /**
  * Autocompletion for the field order-service-invoice-position-basic-number.
  */
 $(function() {
-	$("#order-service-invoice-position-basic-number").autocomplete({
-		minLength: 3,
-		limit: 5,
-		appendMethod:'replace',
-		closeOnBlur: true,
-		source : [
-			function(query, add) {
-				orderApplicationNumber = $('#order-application-number').val();
-				$.getJSON("/order/ajax/provide-service-invoice-positions-basic?data[number]=" + query + "&data[application_technical_short_name]=" + orderApplicationNumber, function(response) {
-					add(response);
-				});
+	$("#order-service-invoice-position-basic-number")
+		.autocomplete({
+			autoFocus : true,
+			delay : 500,
+			minLength : 0,
+			source : function(request, response) {
+				$.get(
+					"/order/ajax/provide-service-invoice-positions-basic?"
+					+ "data[number]=" + request.term
+					+ "&data[application_technical_short_name]=" + $('#order-application-number').val(),
+					{},
+					function(data) {
+						response(data.slice(0, 10));
+					}
+				);
 			}
-		],
+		}).on('focus', function(event) {
+			console.log(new Date());
+			console.log($(this));
+		$(this).autocomplete("search", this.value);
 	});
 });
 /**
  * Autocompletion for the field order-service-invoice-position-personal-number.
  */
 $(function() {
-	$("#order-service-invoice-position-personal-number").autocomplete({
-		minLength: 3,
-		limit: 5,
-		appendMethod:'replace',
-		closeOnBlur: true,
-		source : [
-			function(query, add) {
-				orderApplicationNumber = $('#order-application-number').val();
-				$.getJSON("/order/ajax/provide-service-invoice-positions-personal?data[number]=" + query + "&data[application_technical_short_name]=" + orderApplicationNumber, function(response) {
-					add(response);
-				});
+	$("#order-service-invoice-position-personal-number")
+		.autocomplete({
+			autoFocus : true,
+			delay : 500,
+			minLength : 0,
+			source : function(request, response) {
+				$.get(
+					"/order/ajax/provide-service-invoice-positions-personal?"
+					+ "data[number]=" + request.term
+					+ "&data[application_technical_short_name]=" + $('#order-application-number').val(),
+					{},
+					function(data) {
+						response(data.slice(0, 10));
+					}
+				);
 			}
-		],
+		}).on('focus', function(event) {
+			console.log(new Date());
+			console.log($(this));
+		$(this).autocomplete("search", this.value);
 	});
 });
