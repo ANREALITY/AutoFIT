3aff8eae8bd1a2a9324e885b3111493ec6df7f9e
automatix <info@automatix.info>
2016-04-12 20:24:54 +0200

    #48 Issue with missing `type` fixed.

--- module/Order/src/Order/Mapper/LogicalConnectionFtgwMapper.php
+++ module/Order/src/Order/Mapper/LogicalConnectionFtgwMapper.php
@@ -11,6 +11,8 @@ class LogicalConnectionFtgwMapper extends AbstractLogicalConnectionMapper
     public function __construct(AdapterInterface $dbAdapter, HydratorInterface $hydrator, LogicalConnection $prototype)
     {
         parent::__construct($dbAdapter, $hydrator, $prototype);
+
+        $this->type = LogicalConnection::TYPE_FTGW;
     }
 
 }
