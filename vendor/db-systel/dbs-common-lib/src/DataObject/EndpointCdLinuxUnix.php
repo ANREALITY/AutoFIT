3f4f21331843c65c7cc5ec2764b73b167cb5be02
automatix <info@automatix.info>
2016-04-21 15:01:32 +0200

    #35 `DataObject` `EndpointCdLinuxUnix` added.

---
+++ vendor/db-systel/dbs-common-lib/src/DataObject/EndpointCdLinuxUnix.php
@@ -0,0 +1,131 @@
+<?php
+namespace DbSystel\DataObject;
+
+class EndpointCdLinuxUnix extends AbstractEndpoint
+{
+
+    const TRANSMISSION_TYPE_TXT = 'txt';
+
+    const TRANSMISSION_TYPE_BIN = 'bin';
+
+    /**
+     *
+     * @var string
+     */
+    protected $username;
+
+    /**
+     *
+     * @var string
+     */
+    protected $folder;
+
+    /**
+     *
+     * @var string
+     */
+    protected $transmissionType;
+
+    /**
+     *
+     * @var string
+     */
+    protected $transmissionInterval;
+
+    /**
+     *
+     * @var IncludeParameterSet
+     */
+    protected $includeParameterSet;
+
+    /**
+     *
+     * @return the $username
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     *
+     * @param string $username            
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     *
+     * @return the $folder
+     */
+    public function getFolder()
+    {
+        return $this->folder;
+    }
+
+    /**
+     *
+     * @param string $folder            
+     */
+    public function setFolder($folder)
+    {
+        $this->folder = $folder;
+    }
+
+    /**
+     *
+     * @return the $transmissionType
+     */
+    public function getTransmissionType()
+    {
+        return $this->transmissionType;
+    }
+
+    /**
+     *
+     * @param string $transmissionType            
+     */
+    public function setTransmissionType($transmissionType)
+    {
+        $this->transmissionType = $transmissionType;
+    }
+
+    /**
+     *
+     * @return the $transmissionInterval
+     */
+    public function getTransmissionInterval()
+    {
+        return $this->transmissionInterval;
+    }
+
+    /**
+     *
+     * @param string $transmissionInterval            
+     */
+    public function setTransmissionInterval($transmissionInterval)
+    {
+        $this->transmissionInterval = $transmissionInterval;
+    }
+
+    /**
+     *
+     * @return the $includeParameterSet
+     */
+    public function getIncludeParameterSet()
+    {
+        return $this->includeParameterSet;
+    }
+
+    /**
+     *
+     * @param IncludeParameterSet $includeParameterSet
+     */
+    public function setIncludeParameterSet(IncludeParameterSet $includeParameterSet)
+    {
+        $this->includeParameterSet = $includeParameterSet;
+    }
+
+}
