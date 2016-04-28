3f3ea87ea8448d229376985019f27c3fe63e8534
automatix <info@automatix.info>
2016-04-28 19:12:45 +0200

    #80 #91 Minor code improvements: comments, using constants.

--- public/js/input-lists-management.js
+++ public/js/input-lists-management.js
@@ -1,6 +1,8 @@
 /**
- * Dynamic adding of notification field(-set-)s for.
+ * Dynamic adding of notification field(-set-)s.
  */
+// Constants are supported in IE from v11. http://caniuse.com/#search=const
+const NOTIFICATIONS_MAX_NUMBER = 5;
 $(document).ready(function() {
 	addNotificationButton = $('#add-notification-button');
 	addNotificationButton.on('click', addNotification);
@@ -9,11 +11,16 @@ function addNotification() {
     var currentCount = $('#fieldgroup-logical_connection fieldset:first > fieldset').length;
     var template = $('#fieldgroup-logical_connection fieldset:first > span').data('template');
     template = template.replace(/__index__/g, currentCount);
-    if (currentCount < 5) {
+    if (currentCount < NOTIFICATIONS_MAX_NUMBER) {
         $('#fieldgroup-logical_connection fieldset:first').append(template);
     }
     return false;
 }
+/**
+ * Dynamic adding of include parameter fields.
+ */
+//Constants are supported in IE from v11. http://caniuse.com/#search=const
+const INCLUDE_PARAMETERS_MAX_NUMBER = 5;
 $(document).ready(function() {
 	addIncludeParameterButton = $('#add-include-parameter-button');
 	addIncludeParameterButton.on('click', addIncludeParameter);
@@ -22,7 +29,7 @@ function addIncludeParameter() {
     var currentCount = $('#fieldgroup-specific-endpoint-source .include-parameters fieldset:first > fieldset').length;
     var template = $('#fieldgroup-specific-endpoint-source .include-parameters fieldset:first > span').data('template');
     template = template.replace(/__index__/g, currentCount);
-    if (currentCount < 5) {
+    if (currentCount < INCLUDE_PARAMETERS_MAX_NUMBER) {
         $('#fieldgroup-specific-endpoint-source .include-parameters fieldset:first').append(template);
     }
     return false;
