cbc159dd0195188d7ff9f5b3afb3dde33d5c2999
automatix <info@automatix.info>
2016-04-30 20:25:52 +0200

    #85 Custom `Form` related classes. It doesn't work. Just for the history and to directly remove again.

---
+++ vendor/db-systel/dbs-common-lib/src/Form/Element/MultiCheckbox.php
@@ -0,0 +1,8 @@
+<?php
+namespace DbSystel\Form\Element;
+
+use DbSystel\Form\Element\Checkbox;
+
+class MultiCheckbox extends Checkbox
+{
+}
---
+++ vendor/db-systel/dbs-common-lib/src/Form/View/Helper/FormMultiCheckbox.php
@@ -0,0 +1,9 @@
+<?php
+namespace DbSystel\Form\View\Helper;
+
+use Zend\Form\View\Helper\FormMultiCheckbox as ZendFormMultiCheckbox;
+use DbSystel\Form\Element\MultiCheckbox as MultiCheckboxElement;
+
+class FormMultiCheckbox extends ZendFormMultiCheckbox
+{
+}
