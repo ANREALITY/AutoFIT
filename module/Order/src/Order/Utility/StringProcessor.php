484b8ee451a73ef395db25cc6cca57ec00198fae
automatix <info@automatix.info>
2016-05-04 12:02:31 +0200

    #30 `StringProcessor#strReplaceLast(...)` added.

--- module/Order/src/Order/Utility/StringProcessor.php
+++ module/Order/src/Order/Utility/StringProcessor.php
@@ -3,14 +3,24 @@ namespace Order\Utility;
 
 class StringProcessor
 {
-    public function endsWith($haystack, $needle) {
+    public function endsWith($haystack, $needle)
+    {
         // search forward starting from end minus needle length characters
-        $haystackEndsWithNeedle = false; 
+        $haystackEndsWithNeedle = false;
         $expectedNeedlePosition = strlen($haystack) - strlen($needle);
         $actualNeedlePosition = strpos($haystack, $needle, $expectedNeedlePosition);
         
         $return = $needle === '' || ($expectedNeedlePosition >= 0 && $actualNeedlePosition !== false);
 
         return $return;
+    }
+
+    public function strReplaceLast($search, $replace, $subject, &$count = null)
+    {
+        $position = strrpos($subject, $search);
+        if($position !== false){
+            $subject = substr_replace($subject, $replace, $position, strlen($search));
+        }
+        return $subject;
     }
 }
