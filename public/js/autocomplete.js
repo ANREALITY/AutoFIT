8c9084b10191efb99cb9e3e774b71309f1e3ac16
automatix <info@automatix.info>
2016-05-10 16:19:48 +0200

    #97 Comments improved slightly. Obsolete code removed.

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -140,7 +140,7 @@ $(function() {
 	});
 });
 /**
- * Autocompletion for the field physical-connection-source-endpoint-source-application-number.
+ * Autocompletion for the field physical-connection-source-endpoint-target-application-number.
  */
 $(function() {
 	var physicalConnectionSourceEndpointTargetApplicationNumber = $('input[name="file_transfer_request[logical_connection][physical_connection_source][endpoint_target][application][technical_short_name]"]');
@@ -192,63 +192,11 @@ $(function() {
 	});
 });
 /**
- * Autocompletion for the field physical-connection-source-endpoint-source-server-name.
+ * Autocompletion for the field physical-connection-(source|target)-endpoint-(source|target)-server-name.
  */
 $(function() {
 	var physicalConnectionSourceEndpointSourceServerName = $('.autocomplete-server');
 	physicalConnectionSourceEndpointSourceServerName
-		.autocomplete({
-			autoFocus : true,
-			delay : 500,
-			minLength : 0,
-			source : function(request, response) {
-				$.get(
-					"/order/ajax/provide-servers?"
-					+ "data[name]=" + request.term,
-					{},
-					function(data) {
-						response(data.slice(0, 10));
-					}
-				);
-			}
-		}).on('focus', function(event) {
-			console.log(new Date());
-			console.log($(this));
-		$(this).autocomplete("search", this.value);
-	});
-});
-/**
- * Autocompletion for the field physical-connection-source-endpoint-source-server-number.
- */
-$(function() {
-	var physicalConnectionSourceEndpointTargetServerName = $('input[name="file_transfer_request[logical_connection][physical_connection_source][endpoint_target][server][name]"]');
-	physicalConnectionSourceEndpointTargetServerName
-		.autocomplete({
-			autoFocus : true,
-			delay : 500,
-			minLength : 0,
-			source : function(request, response) {
-				$.get(
-					"/order/ajax/provide-servers?"
-					+ "data[name]=" + request.term,
-					{},
-					function(data) {
-						response(data.slice(0, 10));
-					}
-				);
-			}
-		}).on('focus', function(event) {
-			console.log(new Date());
-			console.log($(this));
-		$(this).autocomplete("search", this.value);
-	});
-});
-/**
- * Autocompletion for the field physical-connection-target-endpoint-target-server-number.
- */
-$(function() {
-	var physicalConnectionTargetEndpointTargetServerName = $('input[name="file_transfer_request[logical_connection][physical_connection_target][endpoint_target][server][name]"]');
-	physicalConnectionTargetEndpointTargetServerName
 		.autocomplete({
 			autoFocus : true,
 			delay : 500,
