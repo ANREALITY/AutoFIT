dc5b316df9a182488d92be70a22a9e374097da7b
automatix <info@automatix.info>
2016-05-19 18:46:23 +0200

    #99 Enabling/disabling of the "nice error views" implemented. The error config template completed.

--- module/ErrorHandling/src/ErrorHandling/Handler/ErrorHandler.php
+++ module/ErrorHandling/src/ErrorHandling/Handler/ErrorHandler.php
@@ -50,39 +50,43 @@ class ErrorHandler
         // @todo Make it dynamic! Since not every user should be able to see the technical error message.
         $userIsAdmin = true;
         $message = null;
-        if ($userIsAdmin) {
-            @trigger_error($exception);
-            $lastErrorData = error_get_last();
-            $message = <<<MESSAGE
+        if (! isset($this->config['nice_error_pages_enabled']) || ! $this->config['nice_error_pages_enabled']) {
+            trigger_error($exception);
+        } else {
+            if ($userIsAdmin) {
+                trigger_error($exception);
+                $lastErrorData = error_get_last();
+                $message = <<<MESSAGE
 TYPE: {$lastErrorData['type']}
 MESSAGE: {$lastErrorData['message']}
 FILE: {$lastErrorData['file']}
 LINE: {$lastErrorData['line']}
 MESSAGE;
+            }
+            ob_start();
+            xdebug_var_dump(debug_backtrace());
+            $debugBacktrace = ob_get_clean();
+            $output = [
+                // header
+                $this->translator->translate('An error occured. Please try later again.'),
+                // error reference
+                sprintf($this->translator->translate('Error reference: %s.'), $errorReference),
+                // error info
+                $message,
+                // debug backtrace
+                $debugBacktrace
+            ];
+            $errorViewPath = $this->config['fatal_error_view'];
+            $body = file_get_contents($errorViewPath);
+            $body = str_replace(
+                [
+                    '%__HEADER__%',
+                    '%__ERROR_REFERENCE__%',
+                    '%__ERROR_INFO__%',
+                    '%__DEBUG_BACKTRACE__%'
+                ], $output, $body);
+            echo $body;
         }
-        ob_start();
-        xdebug_var_dump(debug_backtrace());
-        $debugBacktrace = ob_get_clean();
-        $output = [
-            // header
-            $this->translator->translate('An error occured. Please try later again.'),
-            // error reference
-            sprintf($this->translator->translate('Error reference: %s.'), $errorReference),
-            // error info
-            $message,
-            // debug backtrace
-            $debugBacktrace
-        ];
-        $errorViewPath = $this->config['fatal_error_view'];
-        $body = file_get_contents($errorViewPath);
-        $body = str_replace(
-            [
-                '%__HEADER__%',
-                '%__ERROR_REFERENCE__%',
-                '%__ERROR_INFO__%',
-                '%__DEBUG_BACKTRACE__%'
-            ], $output, $body);
-        echo $body;
         error_log($exception);
     }
 
