c2ee5b037cd9fa50fa88fccfc86f5af70bb759e3
automatix <info@automatix.info>
2016-07-20 17:00:32 +0200

    Error handling configs corrected.

--- config/autoload/errors.global.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'errors' => [],
-    'view_manager' => [
-        'template_map' => [
-            'default_zf_error_view' => __DIR__ . '/../../module/Application/view/error/index.phtml',
-        ]
-    ]
-];
