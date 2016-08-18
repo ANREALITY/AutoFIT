cb2805bb9caad48e24bfe44c9b3e8fae4d6b934d
automatix <info@automatix.info>
2016-08-18 17:49:55 +0200

    #142 `Fiedset`: `ServerCommonFieldset`.

--- module/Order/src/Order/Form/Fieldset/ServerCommonFieldset.php
+++ module/Order/src/Order/Form/Fieldset/ServerCommonFieldset.php
@@ -6,4 +6,22 @@ use Zend\Form\Fieldset;
 class ServerCommonFieldset extends AbstractServerFieldset
 {
 
+    public function init()
+    {
+        $this->add(
+            [
+                'type' => 'text',
+                'name' => 'name',
+                'options' => [
+                    'label' => _('server\'s node name'),
+                    'label_attributes' => [
+                        'class' => 'col-md-12'
+                    ]
+                ],
+                'attributes' => [
+                    'class' => 'form-control field-server-node-name'
+                ]
+            ]);
+    }
+
 }
