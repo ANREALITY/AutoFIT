7e841b4e91a9a5730868a7bf03bd97a57a20c85b
automatix <info@automatix.info>
2016-08-22 09:43:18 +0200

    #126 Field `(source|target).customer_name` disabled.

--- public/js/ftgwselfservice-source-hack.js
+++ public/js/ftgwselfservice-source-hack.js
@@ -1,9 +1,18 @@
 /**
  * A hack for FTGW Self-Service to disable the irrelevant source basic endpoint fields.
  */
+fieldEndpointSourceCustomerName = $('#fieldgroup-basic-endpoint-source .field-customer-name');
+
 $(document).ready(function() {
 	fieldEndpointSourceServerPlace = $('#fieldgroup-basic-endpoint-source .field-server-place');
 	fieldEndpointSourceServerPlace.val('external').attr('checked', true);
 	fieldEndpointSourceServerPlace.parent().parent().fadeOut();
 	fieldEndpointSourceServerPlace.filter(':checked').trigger('change');
+
+	disableNeedlessSourceFields();
 });
+
+function disableNeedlessSourceFields() {
+	fieldEndpointSourceCustomerName.val('');
+	fieldEndpointSourceCustomerName.parent().fadeOut('slow');
+}
