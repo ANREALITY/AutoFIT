8a5babaa9efa610a8a195bdd9afd57dc6ff2e7c5
automatix <info@automatix.info>
2016-08-23 10:10:45 +0200

    #140 The field `folder` is not mandatory anymore (since it is sometimes disabled) for the `EndpointCdTandemTargetFieldset`.

--- module/Order/src/Order/Form/Fieldset/EndpointCdTandemTargetFieldset.php
+++ module/Order/src/Order/Form/Fieldset/EndpointCdTandemTargetFieldset.php
@@ -28,7 +28,6 @@ class EndpointCdTandemTargetFieldset extends AbstractEndpointCdTandemFieldset
                     ]
                 ],
                 'attributes' => [
-                    'required' => 'required',
                     'class' => 'form-control field-folder'
                 ]
             ]);
