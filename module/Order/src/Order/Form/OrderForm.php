ada4382b838a0f7be81c92e656a900eccec7f900
automatix <info@automatix.info>
2016-08-31 16:00:16 +0200

    #141 The check, whether at least one -- an internal server, an external server, or a cluster -- is set on every form's "side" (`source`/`target`), is implemented.

--- module/Order/src/Order/Form/OrderForm.php
+++ module/Order/src/Order/Form/OrderForm.php
@@ -2,11 +2,21 @@
 namespace Order\Form;
 
 use Zend\Form\Form;
+use DbSystel\Validator\MinOneNotEmpty;
+use Order\Form\Fieldset\EndpointFtgwSelfServiceSourceFieldset;
+use Order\Form\Fieldset\EndpointCdLinuxUnixSourceFieldset;
+use Order\Form\Fieldset\EndpointCdLinuxUnixTargetFieldset;
+use Order\Form\Fieldset\EndpointFtgwSelfServiceTargetFieldset;
+use Order\Form\Fieldset\AbstractEndpointFieldset;
+use Order\Form\Fieldset\EndpointCdWindowsShareSourceFieldset;
+use Order\Form\Fieldset\EndpointCdWindowsShareTargetFieldset;
 
 class OrderForm extends Form
 {
 
     protected $fileTransferRequestFieldsetServiceName;
+
+    protected $errorMessages = [];
 
     public function __construct($name = null, $options = [], string $fileTransferRequestFieldsetServiceName)
     {
@@ -36,6 +46,86 @@ class OrderForm extends Form
                     'class' => 'btn btn-default'
                 ]
             ]);
+    }
+
+    public function getErrorMessages()
+    {
+        return $this->errorMessages;
+    }
+
+    public function getMessages($elementName = null)
+    {
+        if ($elementName) {
+            $messages = parent::getMessages($elementName);
+        } else {
+            $messages = array_merge(parent::getMessages($elementName), $this->getErrorMessages());
+        }
+        return $messages;
+    }
+
+    public function addErrorMessage($message)
+    {
+        $this->errorMessages[] = $message;
+        return $this;
+    }
+
+    public function isValid() {
+        $isValid = $isValidBasic = parent::isValid();
+        $logicalConnectionFieldset = $this->get('file_transfer_request')->get('logical_connection');
+        if ($logicalConnectionFieldset->has('physical_connection_end_to_end')) {
+            $physicalConnectionCdEndToEndFieldset = $logicalConnectionFieldset->get('physical_connection_end_to_end');
+            $endpointSourceFieldset = $physicalConnectionCdEndToEndFieldset->get('endpoint_source');
+            $endpointTargetFieldset = $physicalConnectionCdEndToEndFieldset->get('endpoint_target');
+        } else {
+            $physicalConnectionFtgwEndToMiddleFieldset = $logicalConnectionFieldset->get('physical_connection_end_to_middle');
+            $physicalConnectionFtgwMiddleToEndFieldset = $logicalConnectionFieldset->get('physical_connection_middle_to_end');
+            $endpointSourceFieldset = $physicalConnectionFtgwEndToMiddleFieldset->get('endpoint_source');
+            $endpointTargetFieldset = $physicalConnectionFtgwMiddleToEndFieldset->get('endpoint_target');
+        }
+        $isValidEndpintSource = $this->validateMinOneNotEmptyValidatorSource($endpointSourceFieldset);
+        $isValidEndpintTarget = $this->validateMinOneNotEmptyValidatorTarget($endpointTargetFieldset);
+        $isValid = $isValidBasic && $isValidEndpintSource && $isValidEndpintTarget;
+        return $isValid;
+    }
+
+    protected function validateMinOneNotEmptyValidatorSource(AbstractEndpointFieldset $endpointSourceFieldset)
+    {
+        $elementsSource = [];
+        if (! $endpointSourceFieldset instanceof EndpointFtgwSelfServiceSourceFieldset) {
+            $elementsSource[] = $endpointSourceFieldset->get('endpoint_server_config')->get('server')->get('name');
+        }
+        if (! $endpointSourceFieldset instanceof EndpointCdWindowsShareSourceFieldset) {
+            $elementsSource[] = $endpointSourceFieldset->get('external_server')->get('name');
+        }
+        if ($endpointSourceFieldset instanceof EndpointCdLinuxUnixSourceFieldset) {
+            $elementsSource[] = $endpointSourceFieldset->get('endpoint_cluster_config')->get('cluster')->get('id');
+        }
+        $minOneNotEmptyValidatorSource = new MinOneNotEmpty(['elements' => $elementsSource]);
+        $isValid = $minOneNotEmptyValidatorSource->isValid(null);
+        if (! $isValid) {
+            $this->addErrorMessage('Neither a server (internal/external), nor a cluster is defined for the source endpoint.');
+        }
+        return $isValid;
+    }
+
+    protected function validateMinOneNotEmptyValidatorTarget(AbstractEndpointFieldset $endpointTargetFieldset)
+    {
+        $elementsTarget = [];
+        if (! $endpointTargetFieldset instanceof EndpointFtgwSelfServiceTargetFieldset) {
+            $elementsTarget[] = $endpointTargetFieldset->get('endpoint_server_config')->get('server')->get('name');
+        }
+        if (! $endpointTargetFieldset instanceof EndpointCdWindowsShareTargetFieldset) {
+            $elementsTarget[] = $endpointTargetFieldset->get('external_server')->get('name');
+        }
+        if ($endpointTargetFieldset instanceof EndpointCdLinuxUnixTargetFieldset) {
+            $elementsTarget[] = $endpointTargetFieldset->get('endpoint_cluster_config')->get('cluster')->get('id');
+        }
+        $minOneNotEmptyValidatorTarget = new MinOneNotEmpty(['elements' => $elementsTarget]);
+        $isValid = $minOneNotEmptyValidatorTarget->isValid(null);
+        if (! $isValid) {
+            $this->addErrorMessage('Neither a server (internal/external), nor a cluster is defined for the target endpoint.');
+        }
+        return $isValid;
     }
 
 }
