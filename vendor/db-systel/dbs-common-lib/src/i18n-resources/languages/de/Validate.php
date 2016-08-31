2c7fdef84ff6545df8b9fb569e8400302651623d
automatix <info@automatix.info>
2016-08-31 16:01:15 +0200

    #141 translations

--- vendor/db-systel/dbs-common-lib/src/i18n-resources/languages/de/Validate.php
+++ vendor/db-systel/dbs-common-lib/src/i18n-resources/languages/de/Validate.php
@@ -2,6 +2,8 @@
 return [
     // MasterData\Validator\Db\ServerNotInUseForCd
     "The Connect:Direct settings for this server already defined." => "Die Connect:Direct-Einstellungen für diesen Server bereits festgelegt.",
-
+    // Order\Form\OrderForm
+    "Neither a server (internal/external), nor a cluster is defined for the source endpoint." => "Es ist weder ein Server (intern/extern), noch ein Cluster für den Quellenendpoint angegeben.",
+    "Neither a server (internal/external), nor a cluster is defined for the target endpoint." => "Es ist weder ein Server (intern/extern), noch ein Cluster für den Zielendpoint angegeben.",
     // ...
 ];
