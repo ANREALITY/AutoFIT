b1cc1fbbf627a412696ef29e468d61758fd19eb2
automatix <info@automatix.info>
2016-09-12 16:13:40 +0200

    #155 `PaginatorAdapter`

---
+++ module/Order/src/Order/Paginator/Adapter/FileTransferRequestPaginatorAdapter.php
@@ -0,0 +1,80 @@
+<?php
+namespace Order\Paginator\Adapter;
+
+use Zend\Db\Sql\Select;
+use Zend\Paginator\Adapter\DbSelect;
+use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Expression;
+
+class FileTransferRequestPaginatorAdapter extends DbSelect
+{
+
+    /**
+     * The SQL statement contains JOINs,
+     * so that every result entity actually needs multiple rows to be built --
+     * or in other words, every entity takes multiple rows.
+     * That means an "entity oriented" (and not "row oriented") solution was needed.
+     * To acieve this, the method replaces the LIMIT by an IN restriction of the overriden method.
+     *
+     * {@inheritDoc}
+     * @see \Zend\Paginator\Adapter\DbSelect::getItems()
+     */
+    public function getItems($offset, $itemCountPerPage)
+    {
+        $select = clone $this->select;
+        // $select->offset($offset);
+        // $select->limit($itemCountPerPage);
+        $relevantIds = $this->getRelevantIds($offset, $itemCountPerPage);
+        $select->where->in('file_transfer_request.id', $relevantIds);
+
+        $statement = $this->sql->prepareStatementForSqlObject($select);
+        $result    = $statement->execute();
+
+        $resultSet = clone $this->resultSetPrototype;
+        $resultSet->initialize($result);
+
+        return iterator_to_array($resultSet);
+    }
+
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \Zend\Paginator\Adapter\DbSelect::count()
+     */
+    public function count()
+    {
+        $select = new Select();
+        $select->from('file_transfer_request')->columns([self::ROW_COUNT_COLUMN_NAME => new Expression('COUNT(*)')]);
+
+        $statement = $this->sql->prepareStatementForSqlObject($select);
+        $result    = $statement->execute();
+        $row       = $result->current();
+        $this->rowCount = $row[self::ROW_COUNT_COLUMN_NAME];
+
+        return $this->rowCount;
+    }
+
+    /**
+     * Returns the IDs for the retrieving of objects.
+     *
+     * @param unknown $offset
+     * @param unknown $itemCountPerPage
+     */
+    protected function getRelevantIds($offset, $itemCountPerPage)
+    {
+        $sql = new Sql($this->sql->getAdapter());
+        $select = $sql->select('file_transfer_request');
+        $select->columns(['id']);
+        $select->offset($offset);
+        $select->limit($itemCountPerPage);
+
+        $statement = $this->sql->prepareStatementForSqlObject($select);
+        $result    = $statement->execute();
+
+        $resultArray = iterator_to_array($result);
+        $relevantIds = array_column($resultArray, 'id');
+
+        return $relevantIds;
+    }
+
+}
