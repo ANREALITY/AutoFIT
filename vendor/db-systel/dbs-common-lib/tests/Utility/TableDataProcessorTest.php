9775cbba272f1edc0c3dca76b8be3811c38f6a1a
automatix <info@automatix.info>
2016-09-13 13:45:07 +0200

    #123 Test class for the `TableDataProcessorTest` added.

---
+++ vendor/db-systel/dbs-common-lib/tests/Utility/TableDataProcessorTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Utility;
+
+use DbSystel\Utility\TableDataProcessor;
+
+class TableDataProcessorTest extends \PHPUnit_Framework_TestCase
+{
+
+    private $tableDataProcessor;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->tableDataProcessor = new TableDataProcessor('|');
+    }
+
+    // ...
+
+}
