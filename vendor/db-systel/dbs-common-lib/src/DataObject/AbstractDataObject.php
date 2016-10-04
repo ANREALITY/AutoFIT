1dd379587259920e65ef4f773aa8be89663a01c2
automatix <info@automatix.info>
2016-10-04 14:41:45 +0200

    #42 `AbstractDataObject` `implements` `\JsonSerializable``.

--- vendor/db-systel/dbs-common-lib/src/DataObject/AbstractDataObject.php
+++ vendor/db-systel/dbs-common-lib/src/DataObject/AbstractDataObject.php
@@ -1,7 +1,19 @@
 <?php
 namespace DbSystel\DataObject;
 
-class AbstractDataObject
+class AbstractDataObject implements \JsonSerializable
 {
+
+    public function jsonSerialize()
+    {
+        $vars = get_object_vars($this);
+        $keys = array_keys($vars);
+        $values = array_values($vars);
+        $keysUnderscored = preg_replace_callback('/([A-Z])/', function($matches) {
+            return '_' . strtolower($matches[1]);
+        }, $keys);
+        $varsUnderscored = array_combine($keysUnderscored, $values);
+        return $varsUnderscored;
+    }
 
 }
