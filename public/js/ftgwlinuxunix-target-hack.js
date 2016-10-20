7670ef01cea8ad3c7b2a9a288fd567f6586e8b07
automatix <info@automatix.info>
2016-10-20 22:18:30 +0200

    #185 Now for the FTGW Windows the `ServerPlace` is only `internal`. CLOSE #185

---
+++ public/js/ftgwlinuxunix-target-hack.js
@@ -0,0 +1,9 @@
+/**
+ * A hack for FTGW Linux/Unix to disable the irrelevant target basic endpoint fields.
+ */
+$(document).ready(function() {
+	fieldEndpointTargetServerPlace = $('#fieldgroup-basic-endpoint-target .field-server-place');
+	fieldEndpointTargetServerPlace.val('internal').attr('checked', true);
+	fieldEndpointTargetServerPlace.parent().parent().fadeOut();
+	fieldEndpointTargetServerPlace.filter(':checked').trigger('change');
+});
