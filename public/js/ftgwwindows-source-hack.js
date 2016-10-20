91b4fbf2c26df00d700f64a2c9330d248f24b970
automatix <info@automatix.info>
2016-10-20 21:41:09 +0200

    #186 Now for the FTGW Windows the `ServerPlace` is only `internal`. CLOSE #186

---
+++ public/js/ftgwwindows-source-hack.js
@@ -0,0 +1,9 @@
+/**
+ * A hack for FTGW Windows to disable the irrelevant source basic endpoint fields.
+ */
+$(document).ready(function() {
+	fieldEndpointSourceServerPlace = $('#fieldgroup-basic-endpoint-source .field-server-place');
+	fieldEndpointSourceServerPlace.val('internal').attr('checked', true);
+	fieldEndpointSourceServerPlace.parent().parent().fadeOut();
+	fieldEndpointSourceServerPlace.filter(':checked').trigger('change');
+});
