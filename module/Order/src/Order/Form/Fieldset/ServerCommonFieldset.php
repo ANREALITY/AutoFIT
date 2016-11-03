df7b9f6b2f93535f82a965a333dfde2f1d54b607
automatix <info@automatix.info>
2016-11-03 11:36:52 +0100

    #118 Servers marked as "conditionally required" in the cases it's needed only.

--- module/Order/src/Order/Form/Fieldset/ServerCommonFieldset.php
+++ module/Order/src/Order/Form/Fieldset/ServerCommonFieldset.php
@@ -5,4 +5,14 @@ use Zend\Form\Fieldset;
 
 class ServerCommonFieldset extends AbstractServerFieldset
 {
+
+    public function init()
+    {
+        parent::init();
+
+        $this->get('name')->setLabelAttributes([
+            'class' => 'col-md-12 required-conditionally'
+        ]);
+    }
+
 }
