a86ca83324267cd9269b5a4a849488bbf6fa16c3
automatix <info@automatix.info>
2016-11-17 17:11:42 +0100

    #203 The `environment` request corrected.

--- module/Order/src/Order/Mapper/EnvironmentMapper.php
+++ module/Order/src/Order/Mapper/EnvironmentMapper.php
@@ -58,7 +58,7 @@ class EnvironmentMapper extends AbstractMapper implements EnvironmentMapperInter
                         ]);
                 }
                 if (! empty($condition['application_technical_short_name'])) {
-                    $select->join('service_invoice', 'service_invoice.environment_severity = environment.severity', []);
+                    $select->join('service_invoice', 'service_invoice.environment_severity = environment.severity');
                     $select->join('application',
                         'application.technical_short_name = service_invoice.application_technical_short_name', []);
                     $select->where(
@@ -67,7 +67,6 @@ class EnvironmentMapper extends AbstractMapper implements EnvironmentMapperInter
                         ]);
                 }
                 if (array_key_exists('with_invoice_positions_only', $condition) && $condition['with_invoice_positions_only'] === true) {
-                    $select->join('service_invoice', 'service_invoice.environment_severity = environment.severity');
                     $select->join(['service_invoice_position_basic' => 'service_invoice_position'], new Expression('service_invoice_position_basic.service_invoice_number = service_invoice.number AND service_invoice_position_basic.order_quantity > 0 AND service_invoice_position_basic.status <> "Beendet"'));
                     $select->join(['article_basic' => 'article'], new Expression('service_invoice_position_basic.article_sku = article_basic.sku AND article_basic.type = "basic"'));
                     $select->join(['service_invoice_position_personal' => 'service_invoice_position'], new Expression('service_invoice_position_personal.service_invoice_number = service_invoice.number AND service_invoice_position_personal.order_quantity > 0 AND service_invoice_position_personal.status <> "Beendet"'));
