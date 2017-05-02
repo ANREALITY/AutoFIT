533040a8534d288600506877294108d146c74964
automatix <info@automatix.info>
2017-05-03 00:50:43 +0200

    #240 The `ProtocolSetForProtocolServerFieldset`s refactored (duplicated code removed).

--- module/Order/src/Order/Form/Fieldset/ProtocolSetForProtocolServerSourceFieldset.php
+++ module/Order/src/Order/Form/Fieldset/ProtocolSetForProtocolServerSourceFieldset.php
@@ -6,39 +6,4 @@ use Zend\Form\Fieldset;
 class ProtocolSetForProtocolServerSourceFieldset extends AbstractProtocolSetForProtocolServerFieldset
 {
 
-    public function __construct($name = null, $options = [])
-    {
-        parent::__construct('protocol_set', ['label' => _('protocol')]);
-    }
-
-    public function init()
-    {
-        parent::init();
-
-        $this->add(
-            [
-                'type' => 'radio',
-                'name' => 'protocols',
-                'options' => [
-                    'label' => _('protocols'),
-                    'label_attributes' => [
-                        'class' => 'col-md-1 protocol-field'
-                    ],
-                    'value_options' => $this->getValueOptions()
-                ],
-                'attributes' => [
-                    'required' => 'required'
-                ]
-            ]);
-    }
-
-    protected function getProtocols()
-    {
-        $protocols = parent::getProtocols();
-        if (array_key_exists('WebDAV', $protocols)) {
-            unset($protocols['WebDAV']);
-        }
-        return $protocols;
-    }
-
 }
