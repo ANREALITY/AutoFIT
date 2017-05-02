b3ec7a795725b8fc4609883a9fda39f8a503048c
automatix <info@automatix.info>
2017-05-03 00:51:23 +0200

    #240 The `HTTPs` option for the `ProtocolSetForProtocolServerTargetFieldset` removed.

--- module/Order/src/Order/Form/Fieldset/ProtocolSetForProtocolServerTargetFieldset.php
+++ module/Order/src/Order/Form/Fieldset/ProtocolSetForProtocolServerTargetFieldset.php
@@ -6,4 +6,13 @@ use Zend\Form\Fieldset;
 class ProtocolSetForProtocolServerTargetFieldset extends AbstractProtocolSetForProtocolServerFieldset
 {
 
+    protected function getProtocols()
+    {
+        $protocols = parent::getProtocols();
+        if (array_key_exists('HTTPs', $protocols)) {
+            unset($protocols['HTTPs']);
+        }
+        return $protocols;
+    }
+
 }
