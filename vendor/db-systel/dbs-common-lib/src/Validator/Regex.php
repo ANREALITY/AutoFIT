2b78f1a10780821475dc7056ec29820857eeb673
automatix <info@automatix.info>
2017-05-02 14:31:15 +0200

    #235 The `patternUserFriendly` logic extended by a `patternUserFriendlyNegative`.

--- vendor/db-systel/dbs-common-lib/src/Validator/Regex.php
+++ vendor/db-systel/dbs-common-lib/src/Validator/Regex.php
@@ -10,17 +10,28 @@ class Regex extends ZendRegex
      * @var string
      */
     protected $patternUserFriendly;
+    /**
+     * @var string
+     */
+    protected $patternUserFriendlyNegative;
 
     public function __construct($pattern)
     {
         $this->messageVariables['patternUserFriendly'] = 'patternUserFriendly';
-        $this->messageTemplates[self::NOT_MATCH] =
-            'The input may only contain the following characters: %patternUserFriendly%.'
-        ;
-        parent::__construct($pattern);
+        $this->messageVariables['patternUserFriendlyNegative'] = 'patternUserFriendlyNegative';
         if (array_key_exists('patternUserFriendly', $pattern)) {
             $this->patternUserFriendly = $pattern['patternUserFriendly'];
+            $this->messageTemplates[self::NOT_MATCH] =
+                'The input may only contain the following characters: %patternUserFriendly%.'
+            ;
         }
+        if (array_key_exists('patternUserFriendlyNegative', $pattern)) {
+            $this->patternUserFriendlyNegative = $pattern['patternUserFriendlyNegative'];
+            $this->messageTemplates[self::NOT_MATCH] =
+                'The input may not contain the following characters: %patternUserFriendlyNegative%.'
+            ;
+        }
+        parent::__construct($pattern);
     }
 
 }
