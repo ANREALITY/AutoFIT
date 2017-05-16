9218a80932d688183e6d3f8fe4527c0d51888eda
automatix <info@automatix.info>
2017-05-16 16:53:46 +0200

    #245 Test data for the table `article`.

---
+++ data/database/sample-data/article.sql
@@ -0,0 +1,13 @@
+-- product type CD
+INSERT INTO `article` (`sku`, `description`, `type`, `product_type_name`) VALUES('GMDWP00','MDW Middleware Spezial- verfahrensintegriert Support Filetransfer Bronze,GMDWP00','personal','cd');
+INSERT INTO `article` (`sku`, `description`, `type`, `product_type_name`) VALUES('GMDWP04','MDW Middleware Spezial- verfahrensintegriert Support Filetransfer Silber,GMDWP04','personal','cd');
+INSERT INTO `article` (`sku`, `description`, `type`, `product_type_name`) VALUES('GMDWP08','MDW Middleware Spezial- verfahrensintegriert Support Filetransfer Gold,GMDWP08','personal','cd');
+INSERT INTO `article` (`sku`, `description`, `type`, `product_type_name`) VALUES('GMDWS08','MDW Middleware Spezial- verfahrensintegriert Filetransfer,GMDWS08','basic','cd');
+-- product type FTGW
+INSERT INTO `article` (`sku`, `description`, `type`, `product_type_name`) VALUES('GMDWO44','MDW Middleware Filetransfer Gateway Transfervolumenerweiterung (10 GB),GMDWO44','on-demand','fgw');
+INSERT INTO `article` (`sku`, `description`, `type`, `product_type_name`) VALUES('GMDWO48','MDW Middleware Filetransfer Gateway Transfereinheitenerweiterung (1000 Stk.),GMDWO48','on-demand','fgw');
+INSERT INTO `article` (`sku`, `description`, `type`, `product_type_name`) VALUES('GMDWO52','MDW Middleware Filetransfer Gateway Verschlusselung,GMDWO52','on-demand','fgw');
+INSERT INTO `article` (`sku`, `description`, `type`, `product_type_name`) VALUES('GMDWP44','MDW Middleware Filetransfer Gateway Support Silber,GMDWP44','personal','fgw');
+INSERT INTO `article` (`sku`, `description`, `type`, `product_type_name`) VALUES('GMDWP48','MDW Middleware Filetransfer Gateway Support Gold,GMDWP48','personal','fgw');
+INSERT INTO `article` (`sku`, `description`, `type`, `product_type_name`) VALUES('GMDWP70','MDW Middleware Filetransfer Gateway Support Bronze,GMDWP70','personal','fgw');
+INSERT INTO `article` (`sku`, `description`, `type`, `product_type_name`) VALUES('GMDWS60','MDW Middleware Filetransfer Gateway Basis,GMDWS60','basic','fgw');
