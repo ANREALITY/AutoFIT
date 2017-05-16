a297e3dcecc20b6975587eec78fe4bea2a852a24
automatix <info@automatix.info>
2017-05-16 18:09:53 +0200

    #245 Test data for the table `user`.

---
+++ data/database/sample-data/user.sql
@@ -0,0 +1,5 @@
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('1','undefined','guest','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('2','JohnDoe','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('3','JohnDoe2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('4','JaneSmith','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('5','JaneSmith2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
