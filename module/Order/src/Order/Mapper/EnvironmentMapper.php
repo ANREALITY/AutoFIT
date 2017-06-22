88f1fc905cf9b48d26f2b5dfc66951a2855546b3
automatix <info@automatix.info>
2017-06-22 11:46:28 +0200

    #248 comments

--- module/Order/src/Order/Mapper/EnvironmentMapper.php
+++ module/Order/src/Order/Mapper/EnvironmentMapper.php
@@ -69,8 +69,10 @@ class EnvironmentMapper extends AbstractMapper implements EnvironmentMapperInter
                 if (array_key_exists('with_invoice_positions_only', $condition) && $condition['with_invoice_positions_only'] === true) {
                     $select->join(['service_invoice_position_basic' => 'service_invoice_position'], new Expression('service_invoice_position_basic.service_invoice_number = service_invoice.number AND service_invoice_position_basic.order_quantity > 0 AND service_invoice_position_basic.status <> "Beendet"'));
                     $select->join(['article_basic' => 'article'], new Expression('service_invoice_position_basic.article_sku = article_basic.sku AND article_basic.type = "basic"'));
+                    // If this condition is used sometime again, complete it with product_type_name=cd|fgw for basic.
                     $select->join(['service_invoice_position_personal' => 'service_invoice_position'], new Expression('service_invoice_position_personal.service_invoice_number = service_invoice.number AND service_invoice_position_personal.order_quantity > 0 AND service_invoice_position_personal.status <> "Beendet"'));
                     $select->join(['article_personal' => 'article'], new Expression('service_invoice_position_personal.article_sku = article_personal.sku AND article_personal.type = "personal"'));
+                    // If this condition is used sometime again, complete it with product_type_name=cd|fgw for personal.
                 }
             }
         }
