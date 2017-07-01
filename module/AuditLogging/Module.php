2c7678b9e6332534f1d63a621e752cd12f8e1d10
automatix <info@automatix.info>
2017-07-02 00:06:02 +0200

    #255 The autoloading simplified

--- module/AuditLogging/Module.php
+++ module/AuditLogging/Module.php
@@ -11,9 +11,6 @@ class Module
     public function getAutoloaderConfig()
     {
         return [
-            'Zend\Loader\ClassMapAutoloader' => [
-                __DIR__ . '/../../vendor/composer' . '/' . 'autoload_classmap.php',
-            ],
             'Zend\Loader\StandardAutoloader' => [
                 'namespaces' => [
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
--- module/ErrorHandling/Module.php
+++ module/ErrorHandling/Module.php
@@ -22,9 +22,6 @@ class Module
     public function getAutoloaderConfig()
     {
         return [
-            'Zend\Loader\ClassMapAutoloader' => [
-                __DIR__ . '/../../vendor/composer' . '/' . 'autoload_classmap.php',
-            ],
             'Zend\Loader\StandardAutoloader' => [
                 'namespaces' => [
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__
