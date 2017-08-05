955e39369e04d4286cdfa48259f9f4972e0aaacd
automatix <info@automatix.info>
2017-08-05 16:30:07 +0200

    #264 minor refactoring for a better readability

--- test/Integration/Functional/Frontend/OrderManipulation/EditOrderTest.php
+++ test/Integration/Functional/Frontend/OrderManipulation/EditOrderTest.php
@@ -52,26 +52,35 @@ class EditOrderTest extends AbstractHttpControllerTestCase
         // checking the form
         /** @var OrderForm $orderForm */
         $orderForm = $this->getApplication()->getMvcEvent()->getResult()->getVariable('form', null);
+
+        $fileFransferRequestData = $dispatchParams['file_transfer_request'];
+        $fileFransferRequestFieldset = $orderForm->get('file_transfer_request');
+        $logicalConnectionData = $fileFransferRequestData['logical_connection'];
+        $logicalConnectionFieldset = $fileFransferRequestFieldset->get('logical_connection');
+        $physicalConnectionEndToEndData = $logicalConnectionData['physical_connection_end_to_end'];
+        $physicalConnectionEndToEndFieldset = $logicalConnectionFieldset->get('physical_connection_end_to_end');
+        $endpointSourceData = $physicalConnectionEndToEndData['endpoint_source'];
+        $endpointSourceFieldset = $physicalConnectionEndToEndFieldset->get('endpoint_source');
+
         $this->assertNotNull($orderForm);
         $this->assertInstanceOf(OrderForm::class, $orderForm);
-        $this->assertEquals($orderId, $orderForm->get('file_transfer_request')->get('id')->getValue());
+        $this->assertEquals($orderId, $fileFransferRequestFieldset->get('id')->getValue());
         $this->assertEquals(
-            $dispatchParams['file_transfer_request']['change_number'],
-            $orderForm->get('file_transfer_request')->get('change_number')->getValue()
+            $fileFransferRequestData['change_number'],
+            $fileFransferRequestFieldset->get('change_number')->getValue()
         );
         $this->assertEquals(
-            $dispatchParams['file_transfer_request']['service_invoice_position_basic']['number'],
-            $orderForm->get('file_transfer_request')->get('service_invoice_position_basic')->get('number')->getValue()
+            $fileFransferRequestData['service_invoice_position_basic']['number'],
+            $fileFransferRequestFieldset->get('service_invoice_position_basic')->get('number')->getValue()
         );
         $this->assertEquals(
-            $dispatchParams['file_transfer_request']['service_invoice_position_personal']['number'],
-            $orderForm->get('file_transfer_request')->get('service_invoice_position_personal')->get('number')->getValue()
+            $fileFransferRequestData['service_invoice_position_personal']['number'],
+            $fileFransferRequestFieldset->get('service_invoice_position_personal')->get('number')->getValue()
         );
         $this->assertEquals(
-            $dispatchParams['file_transfer_request']['logical_connection']['physical_connection_end_to_end']['endpoint_source']['endpoint_server_config']['server']['name'],
-            $orderForm->get('file_transfer_request')->get('logical_connection')->get('physical_connection_end_to_end')->get('endpoint_source')->get('endpoint_server_config')->get('server')->get('name')->getValue()
+            $endpointSourceData['endpoint_server_config']['server']['name'],
+            $endpointSourceFieldset->get('endpoint_server_config')->get('server')->get('name')->getValue()
         );
-//        $test = ;
     }
 
     protected function getDispatchUrl(string $connectionType, string $endpointSourceType, string $endpointTargetType = null)
