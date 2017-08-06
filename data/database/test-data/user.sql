da648789695e749fc9b9ba7182cf0db5184a39c1
automatix <info@automatix.info>
2017-08-06 15:56:37 +0200

    #266 Admin user `undefined2` added.

--- data/database/test-data/user.sql
+++ data/database/test-data/user.sql
@@ -1,5 +1,6 @@
 INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('1','undefined','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
-INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('2','JohnDoe','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
-INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('3','JohnDoe2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
-INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('4','JaneSmith','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
-INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('5','JaneSmith2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('2','undefined2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('3','JohnDoe','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('4','JohnDoe2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('5','JaneSmith','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('6','JaneSmith2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
