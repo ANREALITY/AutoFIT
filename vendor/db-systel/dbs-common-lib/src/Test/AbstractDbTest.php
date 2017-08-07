391ddbb17f2c7e829f6984890b2929468a7ac309
automatix <info@automatix.info>
2017-08-07 16:49:05 +0200

    The database connection logic moved from the `AbstractDbTest` to the `DatabaseConnectionTrait`.

--- vendor/db-systel/dbs-common-lib/src/Test/AbstractDbTest.php
+++ vendor/db-systel/dbs-common-lib/src/Test/AbstractDbTest.php
@@ -1,65 +1,16 @@
 <?php
 namespace DbSystel\Test;
 
-use PHPUnit\DbUnit\Database\Connection;
-use PHPUnit\DbUnit\InvalidArgumentException;
-use PHPUnit\DbUnit\TestCase as DbTestCase;
-use PDO;
+use PHPUnit\DbUnit\TestCase;
 
 /**
  * Class AbstractUnitTest
  *
  * @package DbSystel\Test
  */
-abstract class AbstractDbTest extends DbTestCase
+abstract class AbstractDbTest extends TestCase
 {
 
-    /**
-     * @var array
-     */
-    static private $dbConfigs;
-    /**
-     * @var PDO
-     */
-    static private $pdo;
-    /**
-     * @var Connection
-     */
-    private $connection;
-
-    public function __construct($name = null, array $data = [], $dataName = '')
-    {
-        parent::__construct($name, $data, $dataName);
-    }
-
-    /**
-     * @return Connection
-     */
-    public function getConnection()
-    {
-        if (! $this->connection) {
-            if (! self::$dbConfigs) {
-                throw new InvalidArgumentException(
-                    'Set the database configuration first.'
-                    . ' '. 'Use the ' . self::class . '::setDbConfigs(...).'
-                );
-            }
-            if (! self::$pdo) {
-                self::$pdo = new PDO(
-                    self::$dbConfigs['dsn'],
-                    self::$dbConfigs['username'],
-                    self::$dbConfigs['password'],
-                    [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'']
-                );
-            }
-            $this->connection = $this->createDefaultDBConnection(self::$pdo, self::$dbConfigs['database']);
-        }
-        return $this->connection;
-    }
-
-    public static function setDbConfigs(array $dbConfigs)
-    {
-        self::$dbConfigs = $dbConfigs;
-    }
+    use DatabaseConnectionTrait;
 
 }
