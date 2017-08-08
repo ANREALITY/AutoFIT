02f7df810e3dd039e12cba1b32fb67c1009c83c6
automatix <info@automatix.info>
2017-08-08 15:10:32 +0200

    Some dummy users added to the `user.sql`.

--- data/database/test-data/user.sql
+++ data/database/test-data/user.sql
@@ -1,6 +1,12 @@
 INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('1','undefined','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
 INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('2','undefined2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
-INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('3','JohnDoe','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
-INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('4','JohnDoe2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
-INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('5','JaneSmith','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
-INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('6','JaneSmith2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('3','foo','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('4','foo2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('5','bar','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('6','bar2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('7','buz','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('8','buz2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('9','JohnDoe','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('10','JohnDoe2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('11','JaneSmith','member','2016-10-17 17:41:52','2017-04-20 01:21:23');
+INSERT INTO `user` (`id`, `username`, `role`, `created`, `updated`) VALUES('12','JaneSmith2','admin','2016-10-17 17:41:52','2017-04-20 01:21:23');
