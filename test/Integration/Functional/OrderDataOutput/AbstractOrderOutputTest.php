f0c3b7b7d0e852b880002cbb94e4b876b8f96bd9
automatix <info@automatix.info>
2017-08-08 16:09:48 +0200

    #267 The `AbstractOrderOutputTest` implemented. The placeholder file `empty` removed.

---
+++ test/Integration/Functional/OrderDataOutput/AbstractOrderOutputTest.php
@@ -0,0 +1,78 @@
+<?php
+namespace Test\Integration\Functional\OrderDataOutput;
+
+use DbSystel\Test\AbstractControllerTest;
+use DbSystel\Test\DatabaseInitializer;
+use Zend\Db\Sql\Sql;
+use Zend\Http\Request;
+
+abstract class AbstractOrderOutputTest extends AbstractControllerTest
+{
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->setUpDatabase();
+    }
+
+    protected function createOrder(
+        string $connectionType, string $endpointSourceType, string $endpointTargetType = null, $reset = true
+    ) {
+        if ($reset) {
+            $this->reset();
+        }
+        $createUrl = $this->getCreateUrl($connectionType, $endpointSourceType, $endpointTargetType);
+        $createParams = $this->getCreateParams($connectionType, $endpointSourceType, $endpointTargetType);
+        $this->dispatch($createUrl, Request::METHOD_POST, $createParams);
+    }
+
+    protected function getCreateUrl(string $connectionType, string $endpointSourceType, string $endpointTargetType = null)
+    {
+        $endpointTargetType = $endpointTargetType ?: $endpointSourceType;
+        $createUrl = strtolower(
+            '/order/process/create'
+            . '/' . $connectionType
+            . '/' . $endpointSourceType
+            . '/' . $endpointTargetType
+        );
+        return $createUrl;
+    }
+
+    protected function getCreateParams(string $connectionType, string $endpointSourceType, string $endpointTargetType = null)
+    {
+        $endpointTargetType = $endpointTargetType ?: $endpointSourceType;
+        $fixturesRootFolder = $this->getApplicationServiceLocator()->get('config')['fixtures']['folder'];
+        $fixturesFolder = $fixturesRootFolder . '/' . 'order-create-form-data';
+        $fixtureFile = $connectionType . '_' . $endpointSourceType . '_' . $endpointTargetType . '.json';
+        $fixtureFilePath = $fixturesFolder . '/' . $fixtureFile;
+        $fixtureJson = file_get_contents($fixtureFilePath);
+        $createParams = json_decode($fixtureJson, true);
+        return $createParams;
+    }
+
+    protected function setUpDatabase()
+    {
+        /*
+         * Complete database setup for every single test.
+         * It makes the tests much, much slower.
+         * But so we don't need to care about IDs and duplicated entries.
+         */
+        $dbConfigs = $this->getApplicationServiceLocator()->get('Config')['db'];
+        $databaseInitializer = new DatabaseInitializer($dbConfigs);
+        $databaseInitializer->setUp();
+    }
+
+    protected function tearDown()
+    {
+        $reflectionObject = new \ReflectionObject($this);
+        foreach ($reflectionObject->getProperties() as $prop) {
+            if (!$prop->isStatic() && 0 !== strpos($prop->getDeclaringClass()->getName(), 'PHPUnit_')) {
+                $prop->setAccessible(true);
+                $prop->setValue($this, null);
+            }
+        }
+        unset($_SERVER['AUTH_USER']);
+    }
+
+}
