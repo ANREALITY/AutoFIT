86536b9fc9a7232af289a5617d57e23b03a5a30c
automatix <info@automatix.info>
2017-08-11 18:54:37 +0200

    #267 The most important tests for the `list orders` implemented.

--- test/Integration/Functional/OrderDataOutput/ListOrdersTest.php
+++ test/Integration/Functional/OrderDataOutput/ListOrdersTest.php
@@ -44,7 +44,53 @@ class ListOrdersTest extends AbstractOrderOutputTest
         $this->assertInstanceOf(Paginator::class, $paginator);
 
         $this->assertEquals(self::ITEMS_PER_PAGE, $paginator->getCurrentItemCount());
-        $this->assertEquals($this->getOrdersCountForUser('foo'), $paginator->getTotalItemCount());
+        $this->assertEquals(
+            $this->getOrdersCountForUser('foo'),
+            $paginator->getTotalItemCount()
+        );
+        /** @var FileTransferRequest[] $currentItems */
+        $currentItems = $paginator->getCurrentItems();
+        foreach ($currentItems as $currentItem) {
+            $this->assertInstanceOf(FileTransferRequest::class, $currentItem);
+        }
+        $createParamsForCdAs400 = $this->getCreateParams(
+            strtolower(LogicalConnection::TYPE_CD),
+            strtolower(AbstractEndpoint::TYPE_CD_AS400)
+        );
+        $this->assertEquals(
+            $createParamsForCdAs400['file_transfer_request']['change_number'],
+            $currentItems[0]->getChangeNumber()
+        );
+    }
+
+    public function testListOrders()
+    {
+        $this->createOrders();
+
+        $this->reset();
+
+        $username = 'foo';
+        $_SERVER['AUTH_USER'] = $username;
+
+        $listUrl = '/list-orders';
+        $this->dispatch($listUrl);
+        $this->assertResponseStatusCode(Response::STATUS_CODE_200);
+        $this->assertModuleName('Order');
+        $this->assertControllerName('Order\Controller\Process');
+        $this->assertControllerClass('ProcessController');
+        $this->assertMatchedRouteName('list-orders');
+
+        /** @var Paginator $paginator */
+        $paginator = $this->getApplication()->getMvcEvent()->getResult()->getVariable('paginator', null);
+
+        $this->assertNotNull($paginator);
+        $this->assertInstanceOf(Paginator::class, $paginator);
+
+        $this->assertEquals(self::ITEMS_PER_PAGE, $paginator->getCurrentItemCount());
+        $this->assertEquals(
+            $this->getOrdersCountForUser('foo') + $this->getOrdersCountForUser('bar'),
+            $paginator->getTotalItemCount()
+        );
         /** @var FileTransferRequest[] $currentItems */
         $currentItems = $paginator->getCurrentItems();
         foreach ($currentItems as $currentItem) {
