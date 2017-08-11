e72b124912f6123b23bdad7e12545e8db52f2231
automatix <info@automatix.info>
2017-08-11 19:06:49 +0200

    #267 The tests for the `list orders with pagination` implemented.

--- test/Integration/Functional/OrderDataOutput/ListOrdersTest.php
+++ test/Integration/Functional/OrderDataOutput/ListOrdersTest.php
@@ -106,6 +106,49 @@ class ListOrdersTest extends AbstractOrderOutputTest
         );
     }
 
+    public function testListOrdersPagination()
+    {
+        $this->createOrders();
+
+        $this->reset();
+
+        $username = 'foo';
+        $_SERVER['AUTH_USER'] = $username;
+
+        $listUrl = '/list-orders/page/2';
+        $this->dispatch($listUrl);
+        $this->assertResponseStatusCode(Response::STATUS_CODE_200);
+        $this->assertModuleName('Order');
+        $this->assertControllerName('Order\Controller\Process');
+        $this->assertControllerClass('ProcessController');
+        $this->assertMatchedRouteName('list-orders');
+
+        /** @var Paginator $paginator */
+        $paginator = $this->getApplication()->getMvcEvent()->getResult()->getVariable('paginator', null);
+
+        $this->assertNotNull($paginator);
+        $this->assertInstanceOf(Paginator::class, $paginator);
+
+        $this->assertEquals(self::ITEMS_PER_PAGE, $paginator->getCurrentItemCount());
+        $this->assertEquals(
+            $this->getOrdersCountForUser('foo') + $this->getOrdersCountForUser('bar'),
+            $paginator->getTotalItemCount()
+        );
+        /** @var FileTransferRequest[] $currentItems */
+        $currentItems = $paginator->getCurrentItems();
+        foreach ($currentItems as $currentItem) {
+            $this->assertInstanceOf(FileTransferRequest::class, $currentItem);
+        }
+        $createParamsForFtgwCdWindows = $this->getCreateParams(
+            strtolower(LogicalConnection::TYPE_FTGW),
+            strtolower(AbstractEndpoint::TYPE_FTGW_CD_AS400)
+        );
+        $this->assertEquals(
+            $createParamsForFtgwCdWindows['file_transfer_request']['change_number'],
+            $currentItems[0]->getChangeNumber()
+        );
+    }
+
     protected function createOrders()
     {
         foreach ($this->getOrderTypes() as $username => $orderTypes) {
