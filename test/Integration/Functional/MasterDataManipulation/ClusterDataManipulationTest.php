bccc534e234726addc17952a1b3dadd4da115aa3
automatix <info@automatix.info>
2017-08-13 11:18:51 +0200

    #270 Cluster data manipulation tests implemented.

--- test/Integration/Functional/MasterDataManipulation/ClusterDataManipulationTest.php
+++ test/Integration/Functional/MasterDataManipulation/ClusterDataManipulationTest.php
@@ -1,9 +1,42 @@
 <?php
 namespace Test\Integration\Functional\MasterDataManipulation;
 
+use MasterData\Form\ClusterForm;
+use Zend\Http\PhpEnvironment\Response;
+use Zend\Http\Request;
+
 class ClusterDataManipulationTest extends AbstractDataManipulationTest
 {
 
+    public function testInputNoneOutputForm()
+    {
+        $createClusterUrl = '/master-data/cluster/create';
+        $this->dispatch($createClusterUrl);
+        $this->assertResponseStatusCode(Response::STATUS_CODE_200);
+        $this->assertModuleName('MasterData');
+        $this->assertControllerName('MasterData\Controller\Cluster');
+        $this->assertControllerClass('ClusterController');
+        $this->assertMatchedRouteName('master-data/create-cluster');
 
+        /** @var ClusterForm $clusterForm */
+        $clusterForm = $this->getApplication()->getMvcEvent()->getResult()->getVariable('form', null);
+
+        $this->assertNotNull($clusterForm);
+        $this->assertInstanceOf(ClusterForm::class, $clusterForm);
+    }
+
+    public function testInputDataActionSaving()
+    {
+        $formParams = $this->getFormParams(self::FORM_CREATE_CLUSTER);
+
+        $createClusterUrl = '/master-data/cluster/create';
+        $this->dispatch($createClusterUrl, Request::METHOD_POST, $formParams);
+
+        $this->assertEquals(
+            $formParams['cluster']['virtual_node_name'],
+            // The cluster with the ID=1 already existets (created with the basic data setup).
+            $this->retrieveActualData('cluster', 'id', 2)['virtual_node_name']
+        );
+    }
 
 }
