2c660053bf10a7622b5f8246b68c2ad623fe7a6e
automatix <info@automatix.info>
2017-08-13 11:03:16 +0200

    #270 The `AbstractDataManipulationTest` and a `getFormParams(...)` in it added.

---
+++ test/Integration/Functional/MasterDataManipulation/AbstractDataManipulationTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Test\Integration\Functional\MasterDataManipulation;
+
+use DbSystel\Test\AbstractControllerTest;
+
+abstract class AbstractDataManipulationTest extends AbstractControllerTest
+{
+
+    /** @var string */
+    const FORM_EDIT_SERVER = 'edit-server';
+    /** @var string */
+    const FORM_CREATE_CLUSTER = 'create-cluster';
+
+    protected function getFormParams(string $formName)
+    {
+        $fixturesRootFolder = $this->getApplicationServiceLocator()->get('config')['fixtures']['folder'];
+        $fixturesFolder = $fixturesRootFolder . '/' . 'master-data-form-data';
+        $fixtureFile = $formName . '.json';
+        $fixtureFilePath = $fixturesFolder . '/' . $fixtureFile;
+        $fixtureJson = file_get_contents($fixtureFilePath);
+        $formParams = json_decode($fixtureJson, true);
+        return $formParams;
+    }
+
+}
