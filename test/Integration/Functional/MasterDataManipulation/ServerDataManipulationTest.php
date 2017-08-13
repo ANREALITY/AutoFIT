f7b35fac118f437a254216053a22f012fd74fe87
automatix <info@automatix.info>
2017-08-13 11:08:35 +0200

    #270 Server data manipulation tests implemented.

--- test/Integration/Functional/MasterDataManipulation/ServerDataManipulationTest.php
+++ test/Integration/Functional/MasterDataManipulation/ServerDataManipulationTest.php
@@ -1,9 +1,41 @@
 <?php
 namespace Test\Integration\Functional\MasterDataManipulation;
 
+use MasterData\Form\ServerForm;
+use Zend\Http\PhpEnvironment\Response;
+use Zend\Http\Request;
+
 class ServerDataManipulationTest extends AbstractDataManipulationTest
 {
 
+    public function testInputNoneOutputForm()
+    {
+        $editServerUrl = '/master-data/server/edit';
+        $this->dispatch($editServerUrl);
+        $this->assertResponseStatusCode(Response::STATUS_CODE_200);
+        $this->assertModuleName('MasterData');
+        $this->assertControllerName('MasterData\Controller\Server');
+        $this->assertControllerClass('ServerController');
+        $this->assertMatchedRouteName('master-data/edit-server');
 
+        /** @var ServerForm $serverForm */
+        $serverForm = $this->getApplication()->getMvcEvent()->getResult()->getVariable('form', null);
+
+        $this->assertNotNull($serverForm);
+        $this->assertInstanceOf(ServerForm::class, $serverForm);
+    }
+
+    public function testInputDataActionSaving()
+    {
+        $formParams = $this->getFormParams(self::FORM_EDIT_SERVER);
+
+        $editServerUrl = '/master-data/server/edit';
+        $this->dispatch($editServerUrl, Request::METHOD_POST, $formParams);
+
+        $this->assertEquals(
+            $formParams['server']['virtual_node_name'],
+            $this->retrieveActualData('server', 'name', 'epa3')['virtual_node_name']
+        );
+    }
 
 }
