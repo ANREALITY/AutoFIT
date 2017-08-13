31534f3ef5bb15098d1610d5e036a45f1022f46e
automatix <info@automatix.info>
2017-08-13 10:22:15 +0200

    #270 Test classes for testing manipulations on `server`s and `cluster`s added.

---
+++ test/Integration/Functional/MasterDataManipulation/ClusterDataManipulationTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Test\Integration\Functional\MasterDataManipulation;
+
+use DbSystel\Test\AbstractControllerTest;
+
+class ClusterDataManipulationTest extends AbstractControllerTest
+{
+
+
+
+}
---
+++ test/Integration/Functional/MasterDataManipulation/ServerDataManipulationTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Test\Integration\Functional\MasterDataManipulation;
+
+use DbSystel\Test\AbstractControllerTest;
+
+class ServerDataManipulationTest extends AbstractControllerTest
+{
+
+
+
+}
