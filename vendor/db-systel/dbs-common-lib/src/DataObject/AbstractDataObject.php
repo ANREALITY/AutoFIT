b0cc172b895f570780c23ef521bd031bf43bc685
automatix <info@automatix.info>
2017-08-17 19:59:59 +0200

    #275 The `AbstractDataObject#jsonSerialize()` is now working for private members of cub-classes.

--- vendor/db-systel/dbs-common-lib/src/DataObject/AbstractDataObject.php
+++ vendor/db-systel/dbs-common-lib/src/DataObject/AbstractDataObject.php
@@ -11,9 +11,15 @@ class AbstractDataObject implements \JsonSerializable
 
     public function jsonSerialize()
     {
-        $vars = get_object_vars($this);
-        $keys = array_keys($vars);
-        $values = array_values($vars);
+        $reflection = new \ReflectionClass($this);
+        $properties = $reflection->getProperties();
+        $members = [];
+        foreach ($properties as $property) {
+            $property->setAccessible(true);
+            $members[$property->getName()] = $property->getValue($this);
+        }
+        $keys = array_keys($members);
+        $values = array_values($members);
         $keysUnderscored = preg_replace_callback('/([A-Z])/', function($matches) {
             return '_' . strtolower($matches[1]);
         }, $keys);
