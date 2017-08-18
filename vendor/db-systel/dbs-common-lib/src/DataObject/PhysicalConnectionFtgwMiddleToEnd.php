78bde5e29377211df6bc4e5a84c1864529fa6a09
automatix <info@automatix.info>
2017-08-18 12:55:43 +0200

    #276 The `AbstractPhysicalConnectionFieldsetFactory` updated for the `PhysicalConnectionFtgwEndToMiddle` and the new `PhysicalConnectionFtgwMiddleToEnd`.

---
+++ vendor/db-systel/dbs-common-lib/src/DataObject/PhysicalConnectionFtgwMiddleToEnd.php
@@ -0,0 +1,12 @@
+<?php
+namespace DbSystel\DataObject;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PhysicalConnectionFtgwMiddleToEnd
+ */
+class PhysicalConnectionFtgwMiddleToEnd extends AbstractPhysicalConnection
+{
+
+}
