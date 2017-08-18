6cb39742c699180acc157e9539ce381657a245c6
automatix <info@automatix.info>
2017-08-18 23:46:35 +0200

    #275 `EndpointType` added.

---
+++ vendor/db-systel/dbs-common-lib/src/DataObject/EndpointType.php
@@ -0,0 +1,87 @@
+<?php
+namespace DbSystel\DataObject;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * EndpointType
+ */
+class EndpointType extends AbstractDataObject
+{
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $label;
+
+    /**
+     * @param integer $id
+     *
+     * @return EndpointType
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return EndpointType
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $label
+     *
+     * @return EndpointType
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+}
