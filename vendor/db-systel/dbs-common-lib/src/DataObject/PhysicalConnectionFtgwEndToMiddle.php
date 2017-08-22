2197aa460d81b465c8300751651a5c2048df25d7
automatix <info@automatix.info>
2017-08-22 21:14:42 +0200

    #275 The `PhysicalConnectionFtgwEndToMiddle` and the `PhysicalConnectionFtgwMiddleToEnd` became Doctrine entities.

--- vendor/db-systel/dbs-common-lib/src/DataObject/PhysicalConnectionFtgwEndToMiddle.php
+++ vendor/db-systel/dbs-common-lib/src/DataObject/PhysicalConnectionFtgwEndToMiddle.php
@@ -5,6 +5,9 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * PhysicalConnectionFtgwEndToMiddle
+ *
+ * @ORM\Table(name="physical_connection_ftgw_end_to_middle")
+ * @ORM\Entity
  */
 class PhysicalConnectionFtgwEndToMiddle extends AbstractPhysicalConnection
 {
--- vendor/db-systel/dbs-common-lib/src/DataObject/PhysicalConnectionFtgwMiddleToEnd.php
+++ vendor/db-systel/dbs-common-lib/src/DataObject/PhysicalConnectionFtgwMiddleToEnd.php
@@ -5,6 +5,9 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * PhysicalConnectionFtgwMiddleToEnd
+ *
+ * @ORM\Table(name="physical_connection_ftgw_middle_to_end")
+ * @ORM\Entity
  */
 class PhysicalConnectionFtgwMiddleToEnd extends AbstractPhysicalConnection
 {
