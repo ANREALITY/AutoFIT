514ca8b820419cce140e5648a4232d31a4c5e513
automatix <info@automatix.info>
2017-08-24 14:04:27 +0200

    #275 The `EndpointFtgwSelfService` entity completed.

--- vendor/db-systel/dbs-common-lib/src/DataObject/EndpointFtgwSelfService.php
+++ vendor/db-systel/dbs-common-lib/src/DataObject/EndpointFtgwSelfService.php
@@ -5,33 +5,53 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * EndpointFtgwSelfService
+ *
+ * @ORM\Table(
+ *     name="endpoint_ftgw_self_service",
+ *     indexes={
+ *         @ORM\Index(name="fk_endpoint_ftgw_self_service_protocol_set_idx", columns={"protocol_set_id"})
+ *     }
+ * )
+ * @ORM\Entity
  */
 class EndpointFtgwSelfService extends AbstractEndpoint
 {
 
+    /** @var string */
     const CONNECTION_TYPE_INTERNAL = 'internal';
-
+    /** @var string */
     const CONNECTION_TYPE_EXTERNAL = 'external';
-
+    /** @var string */
     const CONNECTION_TYPE_BOTH = 'both';
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="ftgw_username", type="string", length=50, nullable=true)
      */
     private $ftgwUsername;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="mailbox", type="string", length=50, nullable=true)
      */
     private $mailbox;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="connection_type", type="string", nullable=true)
      */
     private $connectionType;
 
     /**
      * @var ProtocolSet
+     *
+     * @ORM\OneToOne(targetEntity="ProtocolSet")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="protocol_set_id", referencedColumnName="id")
+     * })
      */
     private $protocolSet;
 
@@ -100,7 +120,7 @@ class EndpointFtgwSelfService extends AbstractEndpoint
      *
      * @return EndpointFtgwSelfService
      */
-    public function setProtocolSet(ProtocolSet $protocolSet)
+    public function setProtocolSet(ProtocolSet $protocolSet = null)
     {
         $this->protocolSet = $protocolSet;
 
