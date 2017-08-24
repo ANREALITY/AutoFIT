45f6f804955889c09e449e9a8354c363558df783
automatix <info@automatix.info>
2017-08-24 12:57:22 +0200

    #275 The `EndpointCdZos` entity completed.

--- vendor/db-systel/dbs-common-lib/src/DataObject/EndpointCdZos.php
+++ vendor/db-systel/dbs-common-lib/src/DataObject/EndpointCdZos.php
@@ -5,17 +5,32 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * EndpointCdZos
+ *
+ * @ORM\Table(
+ *     name="endpoint_cd_zos",
+ *     indexes={
+ *         @ORM\Index(name="fk_endpoint_cd_zos_file_parameter_set_idx", columns={"file_parameter_set_id"})
+ *     }
+ * )
+ * @ORM\Entity
  */
 class EndpointCdZos extends AbstractEndpoint
 {
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=50, nullable=true)
      */
     private $username;
 
     /**
      * @var FileParameterSet
+     *
+     * @ORM\ÔneToOne(targetEntity="FileParameterSet")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="file_parameter_set_id", referencedColumnName="id")
+     * })
      */
     private $fileParameterSet;
 
@@ -44,7 +59,7 @@ class EndpointCdZos extends AbstractEndpoint
      *
      * @return EndpointCdZos
      */
-    public function setFileParameterSet(FileParameterSet $fileParameterSet)
+    public function setFileParameterSet(FileParameterSet $fileParameterSet = null)
     {
         $this->fileParameterSet = $fileParameterSet;
 
