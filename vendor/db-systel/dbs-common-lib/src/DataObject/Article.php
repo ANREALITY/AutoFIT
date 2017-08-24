f77000020798aad687ef49a106db6061b0c79e51
automatix <info@automatix.info>
2017-08-24 10:28:53 +0200

    #275 The `Article` entity completed.

--- vendor/db-systel/dbs-common-lib/src/DataObject/Article.php
+++ vendor/db-systel/dbs-common-lib/src/DataObject/Article.php
@@ -5,33 +5,53 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Article
+ *
+ * @ORM\Table(
+ *     name="article",
+ *     indexes={@ORM\Index(name="fk_article_product_type_idx", columns={"product_type_name"})
+ * })
+ * @ORM\Entity
  */
 class Article extends AbstractDataObject
 {
 
+    /** @var string */
     const TYPE_BASIC = 'basic';
-
+    /** @var string */
     const TYPE_PERSONAL = 'personal';
-
+    /** @var string */
     const TYPE_ON_DEMAND = 'on-demand';
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="sku", type="string", length=16, nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
      */
     private $sku;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=100, nullable=true)
      */
     private $description;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="type", type="string", nullable=true)
      */
     private $type;
 
     /**
      * @var ProductType
+     *
+     * @ORM\ManyToOne(targetEntity="ProductType")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="product_type_name", referencedColumnName="name")
+     * })
      */
     private $productType;
 
