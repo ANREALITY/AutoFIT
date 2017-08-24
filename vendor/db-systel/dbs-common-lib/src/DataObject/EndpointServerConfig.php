ec5feacbfc6ac4c25072e8a0047f8f9c216fe37c
automatix <info@automatix.info>
2017-08-24 14:33:28 +0200

    #275 The `EndpointServerConfig` entity completed.

--- vendor/db-systel/dbs-common-lib/src/DataObject/EndpointServerConfig.php
+++ vendor/db-systel/dbs-common-lib/src/DataObject/EndpointServerConfig.php
@@ -5,27 +5,47 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * EndpointServerConfig
+ *
+ * @ORM\Table(name="endpoint_server_config", indexes={
+ *     @ORM\Index(
+ *         name="fk_endpoint_server_config_server_idx", columns={"server_name"})
+ *     }
+ * )
+ * @ORM\Entity
  */
 class EndpointServerConfig extends AbstractDataObject
 {
 
     /**
      * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $id;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="dns_address", type="string", length=253, nullable=true)
      */
     private $dnsAddress;
 
     /**
      * @var Server
+     *
+     * @ORM\ManyToOne(targetEntity="Server")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="server_name", referencedColumnName="name")
+     * })
      */
     private $server;
 
     /**
      * @var AbstractEndpoint
+     *
+     * @ORM\OneToOne(targetEntity="AbstractEndpoint", mappedBy="endpointServerConfig")
      */
     private $endpoint;
 
@@ -74,7 +94,7 @@ class EndpointServerConfig extends AbstractDataObject
      *
      * @return EndpointServerConfig
      */
-    public function setServer(Server $server)
+    public function setServer(Server $server = null)
     {
         $this->server = $server;
 
