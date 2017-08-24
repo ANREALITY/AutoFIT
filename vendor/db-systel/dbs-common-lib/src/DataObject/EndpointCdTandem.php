01bc01bc3a0fa53d656acd923e1ba10ed460911d
automatix <info@automatix.info>
2017-08-24 12:09:15 +0200

    #275 The `EndpointCdTandem` entity completed.

--- vendor/db-systel/dbs-common-lib/src/DataObject/EndpointCdTandem.php
+++ vendor/db-systel/dbs-common-lib/src/DataObject/EndpointCdTandem.php
@@ -5,17 +5,24 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * EndpointCdTandem
+ *
+ * @ORM\Table(name="endpoint_cd_tandem")
+ * @ORM\Entity
  */
 class EndpointCdTandem extends AbstractEndpoint
 {
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=50, nullable=true)
      */
     private $username;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="folder", type="string", length=200, nullable=true)
      */
     private $folder;
 
