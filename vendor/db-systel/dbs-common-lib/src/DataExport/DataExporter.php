c33a96be3c16b506cf71a3fc08e3d0cfd6edfc55
automatix <info@automatix.info>
2017-09-07 21:18:32 +0200

    #277 The export reimplemented with the `Symfony Serializer Component`.

--- vendor/db-systel/dbs-common-lib/src/DataExport/DataExporter.php
+++ vendor/db-systel/dbs-common-lib/src/DataExport/DataExporter.php
@@ -2,34 +2,70 @@
 namespace DbSystel\DataExport;
 
 use DbSystel\DataObject\AbstractDataObject;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class DataExporter
 {
 
+    /** @var string */
     const EXPORT_FORMAT_JSON = 'json';
-
+    /** @var string */
     const EXPORT_FORMAT_XML = 'xml';
-
+    /** @var string */
     const XML_DEFAULT_ROOT_ELEMENT = 'file_transfer_request';
-
+    /** @var string */
     const XML_DEFAULT_ELEMENT_NAME = 'item';
+
+    /** @var Serializer */
+    protected $serializer;
+
+    public function __construct()
+    {
+        $normalizer = new ObjectNormalizer();
+        $normalizer->setCircularReferenceLimit(1);
+        $normalizer->setIgnoredAttributes(['__initializer__', '__cloner__', '__isInitialized__']);
+        $normalizer->setCircularReferenceHandler(function ($object) {
+            try {
+                $return = $object->getId();
+            } catch (\Error $exception) {
+                $return = null;
+            }
+            $return = null;
+            return $return;
+        });
+        $normalizers = [$normalizer];
+        $this->serializer = new Serializer($normalizers);
+    }
 
     public function exportToJson(AbstractDataObject $dataObject)
     {
-        return json_encode($dataObject, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        $data = $this->serializer->normalize($dataObject);
+        $data = $this->utf8ize($data);
+        return json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
     }
 
     public function exportToXml(AbstractDataObject $dataObject)
     {
-        $dataObjectVars = json_decode(json_encode($dataObject->jsonSerialize()), true);
+        $data = $this->serializer->normalize($dataObject);
+        $data = $this->utf8ize($data);
         $xml = new \SimpleXMLElement('<' . self::XML_DEFAULT_ROOT_ELEMENT . ' />');
-        $this->arrayToXml($dataObjectVars, $xml);
-        $domxml = new \DOMDocument('1.0');
-        $domxml->preserveWhiteSpace = false;
-        $domxml->formatOutput = true;
-        $domxml->loadXML($xml->asXML());
-        $xmlString = $domxml->saveXML();
+        $this->arrayToXml($data, $xml);
+        $domDocument = dom_import_simplexml($xml)->ownerDocument;
+        $domDocument->formatOutput = true;
+        $xmlString = $domDocument->saveXML();
         return $xmlString;
+    }
+
+    protected function utf8ize($data) {
+        if (is_array($data)) {
+            foreach ($data as $key => $value) {
+                $data[$key] = $this->utf8ize($value);
+            }
+        } else if (is_string ($data)) {
+            return utf8_encode($data);
+        }
+        return $data;
     }
 
     /**
