7cbeb25e6b3eed442135c50059c7417c19ad12a7
automatix <info@automatix.info>
2017-09-08 15:03:06 +0200

    #277 In all `Endpoint` entity classes the obsolete `Export` annotation import statement replaced with the `Groups` annotation import.

--- vendor/db-systel/dbs-common-lib/src/DataObject/EndpointCdWindowsShare.php
+++ vendor/db-systel/dbs-common-lib/src/DataObject/EndpointCdWindowsShare.php
@@ -2,7 +2,7 @@
 namespace DbSystel\DataObject;
 
 use Doctrine\ORM\Mapping as ORM;
-use DbSystel\Annotation\Export;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * EndpointCdWindowsShare
