f86c476c567264be56b7498929a81eaa37558b7c
automatix <info@automatix.info>
2017-09-25 10:08:35 +0200

    Memory limit for tests increased to `1024M`.

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -11,7 +11,7 @@ use Zend\Mvc\Service\ServiceManagerConfig;
 use Zend\ServiceManager\ServiceManager;
 
 error_reporting(E_ALL | E_STRICT);
-ini_set('memory_limit', '896M');
+ini_set('memory_limit', '1024M');
 chdir(__DIR__);
 
 /**
