4f77f08ead730992fe010631dc3939a0436325ee
automatix <info@automatix.info>
2017-11-10 13:19:25 +0100

    #285 The autocomplete for the audit logging moved to a separate file.

---
+++ public/js/audit-logging/search-filter-autocomplete.js
@@ -0,0 +1,52 @@
+/**
+ * Autocompletion for the field username.
+ */
+const AUTOCOMPLETE_USERS_LIMIT = 25;
+$(function() {
+    $(".autocomplete-username")
+        .autocomplete({
+            autoFocus : true,
+            delay : 500,
+            minLength : 0,
+            source : function(request, response) {
+                $.get(
+                    "/audit-logging/ajax/provide-users?"
+                    + "data[username]=" + request.term,
+                    {},
+                    function(data) {
+                        response(data.slice(0, AUTOCOMPLETE_USERS_LIMIT));
+                    }
+                );
+            }
+        }).on('focus', function(event) {
+        console.log(new Date());
+        console.log($(this));
+        $(this).autocomplete("search", this.value);
+    });
+});
+/**
+ * Autocompletion for the field username.
+ */
+const AUTOCOMPLETE_FILE_TRANSFER_REQUESTS_LIMIT = 25;
+$(function() {
+    $(".autocomplete-change-number")
+        .autocomplete({
+            autoFocus : true,
+            delay : 500,
+            minLength : 0,
+            source : function(request, response) {
+                $.get(
+                    "/audit-logging/ajax/provide-file-transfer-requests?"
+                    + "data[change_number]=" + request.term,
+                    {},
+                    function(data) {
+                        response(data.slice(0, AUTOCOMPLETE_FILE_TRANSFER_REQUESTS_LIMIT));
+                    }
+                );
+            }
+        }).on('focus', function(event) {
+        console.log(new Date());
+        console.log($(this));
+        $(this).autocomplete("search", this.value);
+    });
+});
