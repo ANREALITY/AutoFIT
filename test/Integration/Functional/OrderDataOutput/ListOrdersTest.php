7008a64a90d4866bdd83183781b976b9abbb9952
automatix <info@automatix.info>
2017-11-29 15:19:04 +0100

    #303 The `testListMyOrders` test removed.

--- test/Integration/Functional/OrderDataOutput/ListOrdersTest.php
+++ test/Integration/Functional/OrderDataOutput/ListOrdersTest.php
@@ -15,54 +15,6 @@ class ListOrdersTest extends AbstractOrderOutputTest
      */
     const ITEMS_PER_PAGE = 3;
 
-    public function testListMyOrders()
-    {
-        $this->createOrders();
-
-        $this->reset();
-
-        $username = 'foo';
-        $_SERVER['AUTH_USER'] = $username;
-
-        $listUrl = '/order/list-own';
-        $this->dispatch($listUrl);
-        $this->assertResponseStatusCode(Response::STATUS_CODE_200);
-        $this->assertModuleName('Order');
-        $this->assertControllerName('Order\Controller\Process');
-        $this->assertControllerClass('ProcessController');
-        $this->assertMatchedRouteName('order/list-own');
-
-        /** @var Paginator $paginator */
-        $paginator = $this->getApplication()->getMvcEvent()->getResult()->getVariable('paginator', null);
-
-        $this->assertNotNull($paginator);
-        $this->assertInstanceOf(Paginator::class, $paginator);
-
-        $this->assertEquals(self::ITEMS_PER_PAGE, $paginator->getCurrentItemCount());
-        $this->assertEquals(
-            $this->getOrdersCountForUser('foo'),
-            $paginator->getTotalItemCount()
-        );
-        /** @var FileTransferRequest[] $currentItems */
-        $currentItems = $paginator->getCurrentItems();
-        foreach ($currentItems as $currentItem) {
-            $this->assertInstanceOf(FileTransferRequest::class, $currentItem);
-        }
-
-        /** @var User $currentUser */
-        $currentUser = $this->entityManager->getRepository(User::class)
-            ->findOneBy(['username' => $username])
-        ;
-        /** @var FileTransferRequest $latestOrder */
-        $latestOrder = $this->entityManager->getRepository(FileTransferRequest::class)
-            ->findOneBy(['user' => $currentUser], ['created' => 'DESC'])
-        ;
-        $this->assertEquals(
-            $latestOrder->getChangeNumber(),
-            $currentItems[0]->getChangeNumber()
-        );
-    }
-
     public function testListOrders()
     {
         $this->createOrders();
