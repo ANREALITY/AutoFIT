95a18450963618c8f011bc5aad55587a6e82e1e7
automatix <khanataev@gmail.com>
2019-07-24 09:37:00 +0200

    #313 The `ListOrdersTest` and `ShowOrderTest` updated according to the recent changes. The test `ListOrdersTest#testListMyOrders()` restored.

--- test/Integration/Functional/OrderDataOutput/ListOrdersTest.php
+++ test/Integration/Functional/OrderDataOutput/ListOrdersTest.php
@@ -15,13 +15,75 @@ class ListOrdersTest extends AbstractOrderOutputTest
      */
     const ITEMS_PER_PAGE = 3;
 
-    public function testListOrders()
+    public function testListMyOrders()
     {
         $this->createOrders();
 
         $this->reset();
 
         $username = 'foo';
+        $_SERVER['AUTH_USER'] = $username;
+
+        $listUrl = '/order/list-own';
+        $this->dispatch($listUrl);
+        $this->assertResponseStatusCode(Response::STATUS_CODE_200);
+        $this->assertModuleName('Order');
+        $this->assertControllerName('Order\Controller\Process');
+        $this->assertControllerClass('ProcessController');
+        $this->assertMatchedRouteName('order/list-own');
+
+        /** @var Paginator $paginator */
+        $paginator = $this->getApplication()->getMvcEvent()->getResult()->getVariable('paginator', null);
+
+        $this->assertNotNull($paginator);
+        $this->assertInstanceOf(Paginator::class, $paginator);
+
+        $this->assertEquals(self::ITEMS_PER_PAGE, $paginator->getCurrentItemCount());
+        $this->assertEquals(
+            $this->getOrdersCountForUser('foo'),
+            $paginator->getTotalItemCount()
+        );
+        /** @var FileTransferRequest[] $currentItems */
+        $currentItems = $paginator->getCurrentItems();
+        foreach ($currentItems as $currentItem) {
+            $this->assertInstanceOf(FileTransferRequest::class, $currentItem);
+        }
+
+        /** @var User $currentUser */
+        $currentUser = $this->entityManager->getRepository(User::class)
+            ->findOneBy(['username' => $username])
+        ;
+        /** @var FileTransferRequest $latestOrder */
+        $latestOrder = $this->entityManager->getRepository(FileTransferRequest::class)
+            ->findOneBy(['user' => $currentUser], ['created' => 'DESC'])
+        ;
+        $this->assertEquals(
+            $latestOrder->getChangeNumber(),
+            $currentItems[0]->getChangeNumber()
+        );
+    }
+
+    public function testListOrdersAccessDeniedForNonAdmins()
+    {
+        $this->createOrders();
+
+        $this->reset();
+
+        $username = 'foo';
+        $_SERVER['AUTH_USER'] = $username;
+
+        $listUrl = '/order/list';
+        $this->dispatch($listUrl);
+        $this->assertResponseStatusCode(Response::STATUS_CODE_302);
+    }
+
+    public function testListOrders()
+    {
+        $this->createOrders();
+
+        $this->reset();
+
+        $username = 'foo2';
         $_SERVER['AUTH_USER'] = $username;
 
         $listUrl = '/order/list';
@@ -64,7 +126,7 @@ class ListOrdersTest extends AbstractOrderOutputTest
 
         $this->reset();
 
-        $username = 'foo';
+        $username = 'foo2';
         $_SERVER['AUTH_USER'] = $username;
 
         $listUrl = '/order/list/page/2';
--- test/Integration/Functional/OrderDataOutput/ShowOrderTest.php
+++ test/Integration/Functional/OrderDataOutput/ShowOrderTest.php
@@ -3,6 +3,7 @@ namespace Test\Integration\Functional\OrderDataOutput;
 
 use Base\DataObject\EndpointCdLinuxUnix;
 use Base\DataObject\FileTransferRequest;
+use Exception;
 use Order\Service\UserService;
 use Zend\Http\PhpEnvironment\Response;
 
@@ -12,8 +13,10 @@ class ShowOrderTest extends AbstractOrderOutputTest
     /**
      * @dataProvider provideDataForShowOrder
      * @param string $username
+     * @param bool $isOwner
+     * @throws Exception
      */
-    public function testShowOrder(string $username)
+    public function testShowOrder(string $username, bool $isOwner)
     {
         $connectionType = 'cd';
         $endpointSourceType = 'cdlinuxunix';
@@ -28,51 +31,62 @@ class ShowOrderTest extends AbstractOrderOutputTest
         $orderId = 1;
         $showUrl = '/order/show/' . $orderId;
         $this->dispatch($showUrl);
-        $this->assertResponseStatusCode(Response::STATUS_CODE_200);
-        $this->assertModuleName('Order');
-        $this->assertControllerName('Order\Controller\Process');
-        $this->assertControllerClass('ProcessController');
-        $this->assertMatchedRouteName('order/show');
 
-        /** @var FileTransferRequest $fileTransferRequest */
-        $fileTransferRequest = $this->getApplication()->getMvcEvent()->getResult()->getVariable('fileTransferRequest', null);
+        if ($isOwner) {
+            $this->assertResponseStatusCode(Response::STATUS_CODE_200);
+            $this->assertModuleName('Order');
+            $this->assertControllerName('Order\Controller\Process');
+            $this->assertControllerClass('ProcessController');
+            $this->assertMatchedRouteName('order/show');
 
-        $fileTransferRequestData = $createParams['file_transfer_request'];
-        $logicalConnectionData = $fileTransferRequestData['logical_connection'];
-        $logicalConnection = $fileTransferRequest->getLogicalConnection();
-        $physicalConnectionEndToEndData = $logicalConnectionData['physical_connection_end_to_end'];
-        $physicalConnectionEndToEnd = $logicalConnection->getPhysicalConnectionEndToEnd();
-        $endpointSourceData = $physicalConnectionEndToEndData['endpoint_source'];
-        /** @var EndpointCdLinuxUnix $endpointSource */
-        $endpointSource = $physicalConnectionEndToEnd->getEndpointSource();
+            /** @var FileTransferRequest $fileTransferRequest */
+            $fileTransferRequest = $this->getApplication()->getMvcEvent()->getResult()->getVariable('fileTransferRequest', null);
 
-        $this->assertNotNull($fileTransferRequest);
-        $this->assertInstanceOf(FileTransferRequest::class, $fileTransferRequest);
-        $this->assertEquals($orderId, $fileTransferRequest->getId());
-        $this->assertEquals($fileTransferRequestData['change_number'], $fileTransferRequest->getChangeNumber());
-        $this->assertEquals(
-            $logicalConnectionData['notifications'][0]['email'],
-            $logicalConnection->getNotifications()[0]->getEmail()
-        );
-        $this->assertEquals(
-            $fileTransferRequestData['service_invoice_position_basic']['number'],
-            $fileTransferRequest->getServiceInvoicePositionBasic()->getNumber()
-        );
-        $this->assertEquals(
-            $fileTransferRequestData['service_invoice_position_personal']['number'],
-            $fileTransferRequest->getServiceInvoicePositionPersonal()->getNumber()
-        );
-        $this->assertEquals(
-            $endpointSourceData['endpoint_server_config']['server']['name'],
-            $endpointSource->getEndpointServerConfig()->getServer()->getName()
-        );
-        $this->assertEquals(
-            $endpointSourceData['include_parameter_set']['include_parameters'][0]['expression'],
-            $endpointSource->getIncludeParameterSet()->getIncludeParameters()[0]->getExpression()
-        );
+            $fileTransferRequestData = $createParams['file_transfer_request'];
+            $logicalConnectionData = $fileTransferRequestData['logical_connection'];
+            $logicalConnection = $fileTransferRequest->getLogicalConnection();
+            $physicalConnectionEndToEndData = $logicalConnectionData['physical_connection_end_to_end'];
+            $physicalConnectionEndToEnd = $logicalConnection->getPhysicalConnectionEndToEnd();
+            $endpointSourceData = $physicalConnectionEndToEndData['endpoint_source'];
+            /** @var EndpointCdLinuxUnix $endpointSource */
+            $endpointSource = $physicalConnectionEndToEnd->getEndpointSource();
+
+            $this->assertNotNull($fileTransferRequest);
+            $this->assertInstanceOf(FileTransferRequest::class, $fileTransferRequest);
+            $this->assertEquals($orderId, $fileTransferRequest->getId());
+            $this->assertEquals($fileTransferRequestData['change_number'], $fileTransferRequest->getChangeNumber());
+            $this->assertEquals(
+                $logicalConnectionData['notifications'][0]['email'],
+                $logicalConnection->getNotifications()[0]->getEmail()
+            );
+            $this->assertEquals(
+                $fileTransferRequestData['service_invoice_position_basic']['number'],
+                $fileTransferRequest->getServiceInvoicePositionBasic()->getNumber()
+            );
+            $this->assertEquals(
+                $fileTransferRequestData['service_invoice_position_personal']['number'],
+                $fileTransferRequest->getServiceInvoicePositionPersonal()->getNumber()
+            );
+            $this->assertEquals(
+                $endpointSourceData['endpoint_server_config']['server']['name'],
+                $endpointSource->getEndpointServerConfig()->getServer()->getName()
+            );
+            $this->assertEquals(
+                $endpointSourceData['include_parameter_set']['include_parameters'][0]['expression'],
+                $endpointSource->getIncludeParameterSet()->getIncludeParameters()[0]->getExpression()
+            );
+        } else {
+            $this->assertResponseStatusCode(Response::STATUS_CODE_302);
+        }
     }
 
-    public function testHandlingNotExistentOrderId()
+    /**
+     * @dataProvider provideDataForHandlingNotExistentOrderId
+     * @param string $username
+     * @param bool $isAdmin
+     * @throws Exception
+     */
+    public function testHandlingNotExistentOrderId(string $username, bool $isAdmin)
     {
         $connectionType = 'cd';
         $endpointSourceType = 'cdlinuxunix';
@@ -80,15 +94,22 @@ class ShowOrderTest extends AbstractOrderOutputTest
 
         $this->reset();
 
+        $_SERVER['AUTH_USER'] = $username;
+
         // testing the access by the owner
         $orderId = 2;
         $showUrl = '/order/show/' . $orderId;
         $this->dispatch($showUrl);
-        $this->assertResponseStatusCode(Response::STATUS_CODE_404);
-        $this->assertModuleName('Order');
-        $this->assertControllerName('Order\Controller\Process');
-        $this->assertControllerClass('ProcessController');
-        $this->assertMatchedRouteName('order/show');
+
+        if ($isAdmin) {
+            $this->assertResponseStatusCode(Response::STATUS_CODE_404);
+            $this->assertModuleName('Order');
+            $this->assertControllerName('Order\Controller\Process');
+            $this->assertControllerClass('ProcessController');
+            $this->assertMatchedRouteName('order/show');
+        } else {
+            $this->assertResponseStatusCode(Response::STATUS_CODE_302);
+        }
     }
 
     public function provideDataForShowOrder()
@@ -96,9 +117,25 @@ class ShowOrderTest extends AbstractOrderOutputTest
         return [
             'owner' => [
                 'username' => UserService::DEFAULT_MEMBER_USERNAME,
+                'isOwner' => true,
             ],
             'nonOwner' => [
                 'username' => 'foo',
+                'isOwner' => false,
+            ],
+        ];
+    }
+
+    public function provideDataForHandlingNotExistentOrderId()
+    {
+        return [
+            'admin' => [
+                'username' => UserService::DEFAULT_ADMIN_USERNAME,
+                'isAdmin' => true,
+            ],
+            'nonAdmin' => [
+                'username' => UserService::DEFAULT_MEMBER_USERNAME,
+                'isAdmin' => false,
             ],
         ];
     }
